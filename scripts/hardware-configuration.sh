34a8c0be3290f63db6e2d1e7509ea9add120bbb3
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-15 23:40:51 +0000

    Update hardware-configuration.sh

--- scripts/hardware-configuration.sh
+++ scripts/hardware-configuration.sh
@@ -28,11 +28,13 @@ echo "{ config, lib, pkgs, ... }:
 # Loop for filesystems - fstab
 if [ $(findmnt --real --raw | grep '/ ' | wc -l) -gt 0 ]; then
   fstab=$(findmnt --noheadings --real --raw --uniq --nofsroot --output SOURCE,UUID,TARGET,FSTYPE,OPTIONS,FSROOT \
-    | grep -v -e "ro," -e "fuse." -e "gocryptfs" -e "sshfs" -e "/run/media" | sed "s/$/ _/g")
+    | grep -v -e "ro," -e "fuse." -e "gocryptfs" -e "sshfs" -e "/run/media" \
+    | sed -e "s/$/ _/g")
     
 else if [ $(findmnt --real --raw | grep '/mnt ' | wc -l) -gt 0 ]; then
   fstab=$(findmnt --noheadings --real --raw --uniq --nofsroot --output SOURCE,UUID,TARGET,FSTYPE,OPTIONS,FSROOT \
-    | grep -v -e "ro," -e "fuse." -e "gocryptfs" -e "sshfs" -e "/run/media" | sed "s/$/ _/g" | sed "s/\/mnt/\//g")
+    | grep -v -e "ro," -e "fuse." -e "gocryptfs" -e "sshfs" -e "/run/media" \
+    | sed -e "s/$/ _/g" -e "s/\/mnt/\//g" -e "s|//|/|g")
 fi
 fi
 
