f49db0db558263b85762fa83d4d882420be12523
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-15 22:57:55 +0000

    Update n2.sh

--- scripts/deploy/n2.sh
+++ scripts/deploy/n2.sh
@@ -48,18 +48,18 @@ partprobe -s "$disk"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # LUKS root
-cryptsetup luksFormat "$disk"3
+cryptsetup luksFormat "$parts"3
 
-rootid=$(blkid -o value -s UUID "$disk"3)
+rootid=$(blkid -o value -s UUID "$parts"3)
 rootmap="luks-$rootid"
 
-cryptsetup open "$disk"3 "$rootmap"
+cryptsetup open "$parts"3 "$rootmap"
 
 
 # Filesystems
 mkfs.ext4 -F /dev/mapper/"$rootmap"
-mkfs.ext4 -F "$disk"2
-mkfs.vfat -F32 "$disk"1
+mkfs.ext4 -F "$parts"2
+mkfs.vfat -F32 "$parts"1
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -67,8 +67,8 @@ mkfs.vfat -F32 "$disk"1
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 mount --mkdir -o "rw,noatime" /dev/mapper/"$rootmap" /mnt
-mount --mkdir -o "rw,noatime" "$disk"2 /mnt/boot
-mount --mkdir -o "rw,noatime" "$disk"1 /mnt/efi
+mount --mkdir -o "rw,noatime" "$parts"2 /mnt/boot
+mount --mkdir -o "rw,noatime" "$parts"1 /mnt/efi
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
