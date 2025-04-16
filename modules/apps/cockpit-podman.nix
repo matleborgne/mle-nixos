462213aa75ab3f94e519c4c8b3cd31a48fbd493d
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-16 14:17:17 +0000

    Create cockpit-podman.nix

---
+++ modules/apps/cockpit-podman.nix
@@ -0,0 +1,38 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# APPS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Apps modules are the place for customization of individual apps
+# This is not the place for bundles, hardware, desktop, etc.
+# Only for individual applications
+
+{
+
+  options.mle.apps.cockpit-podman.enable = lib.mkOption {
+    description = "Configure COCKIT PODMAN module";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.apps.cockpit-podman.enable {
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    mle.apps.cockpit.enable = lib.mkForce true;
+    mle.misc.podman.enable = lib.mkForce true;
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Activation and customization of APP
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+      
+  };
+}
