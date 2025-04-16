f42c1f3355bff5e525d177303186176231dbf94d
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-16 15:47:19 +0000

    Update podman.nix

--- modules/misc/podman.nix
+++ modules/misc/podman.nix
@@ -40,6 +40,7 @@
 	      # Required for containers under podman-compose to be able to talk to each other.
 	      defaultNetwork.settings.dns_enabled = true;
 	    };
+	  };
 	
 	  environment.systemPackages = with pkgs; [
 	    dive # look into docker image layers
