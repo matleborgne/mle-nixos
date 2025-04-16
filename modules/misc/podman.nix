cac811e436a07ec67367c92529c8d399253353ad
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-16 15:27:54 +0000

    Update podman.nix

--- modules/misc/podman.nix
+++ modules/misc/podman.nix
@@ -28,10 +28,45 @@
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     # lib.optional (mle.apps.cockpit.enable = true) cockpit-podman
-
-		environment.systemPackages = with pkgs; [
-			podman
-		];
+	
+	  virtualisation =  {
+	    containers.enable = true;
+	    oci-containers.backend = "podman";
+	    podman = {
+	      enable = true;
+	      autoPrune.enable = true;
+	      # Create a `docker` alias for podman, to use it as a drop-in replacement
+	      dockerCompat = true;
+	      # Required for containers under podman-compose to be able to talk to each other.
+	      defaultNetwork.settings.dns_enabled = true;
+	    }; # podman
+	
+	    containers.storage.settings = {
+	      storage = {
+		#driver = "btrfs";
+		runroot = "/run/containers/storage";
+		graphroot = "/var/lib/containers/storage";
+		options.overlay.mountopt = "nodev,metacopy=on";
+	      }; # storage
+	    };
+	  }; # virtualisation
+	
+	  users.groups.podman = {
+	    name = "podman";
+	  };
+	
+	  environment.systemPackages = with pkgs; [
+	    dive # look into docker image layers
+	    podman
+	    podman-tui   # Terminal mgmt UI for Podman
+	    passt    # For Pasta rootless networking
+	  ];
+	
+	  # Add 'newuidmap' and 'sh' to the PATH for users' Systemd units. 
+	  # Required for Rootless podman.
+	  systemd.user.extraConfig = ''
+	    DefaultEnvironment="PATH=/run/current-system/sw/bin:/run/wrappers/bin:${lib.makeBinPath [ pkgs.bash ]}"
+	  '';
 
   };  
 }
