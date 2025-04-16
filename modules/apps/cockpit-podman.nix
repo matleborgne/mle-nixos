f03d36a39aa7b2f8db8cd7058fce26c78379c2bd
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-16 14:54:01 +0000

    Update cockpit-podman.nix

--- modules/apps/cockpit-podman.nix
+++ modules/apps/cockpit-podman.nix
@@ -16,13 +16,7 @@
     default = false;
   };
 
-  config = lib.mkIf config.mle.apps.cockpit-podman.enable (
-
-  let
-    cockpit-apps = ../forks/cockpit-podman.nix { inherit pkgs; };
-
-  in
-  {
+  config = lib.mkIf config.mle.apps.cockpit-podman.enable {
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Recursive activation of other mle.<modules>
@@ -36,9 +30,45 @@
     # Activation and customization of APP
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    environment.systemPackages = with pkgs; [
-      cockpit-apps.podman-containers
-    ];
+    environment.systemPackages = 
+      let
+        cockpit-podman = 
+        with import <nixpkgs> {};
+        stdenv.mkDerivation rec {
+          pname = "cockpit-podman";
+          version = "103";
+      
+          src = fetchzip {
+            url = "https://github.com/cockpit-project/${pname}/releases/download/${version}/${pname}-${version}.tar.xz";
+            sha256 = "sha256-0mdqa9w1p6cmli6976v4wi0sw9r4p5prkj7lzfd1877wk11c9c73=";
+          };
+      
+          nativeBuildInputs = [
+            gettext
+          ];
+
+           makeFlags = [ "DESTDIR=$(out)" "PREFIX=" ];
+        
+            postPatch = ''
+              substituteInPlace Makefile \
+                --replace /usr/share $out/share
+              touch pkg/lib/cockpit-po-plugin.js
+              touch dist/manifest.json
+            '';
+        
+            dontBuild = true;
+        
+            meta = with lib; {
+              description = "Cockpit UI for podman containers";
+              license = licenses.lgpl21;
+              homepage = "https://github.com/cockpit-project/cockpit-podman";
+              platforms = platforms.linux;
+              maintainers = with maintainers; [ ];
+            };
+
+          };
+
+        in [ cockpit-podman ];
       
   };
 }
