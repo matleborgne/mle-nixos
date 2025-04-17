f4f0eb7091d6f39080e55fbdb89baf2067a3e25e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-17 15:48:38 +0000

    Update nixos-containers.nix

--- modules/misc/nixos-containers.nix
+++ modules/misc/nixos-containers.nix
@@ -40,40 +40,7 @@
       enable = true;
       internalInterfaces = ["ve-+"];   # "ve-*" if using nftables instead of iptables
       externalInterface = "enp3s0";    # TODO read the interface name
-  # Lazy IPv6 connectivity for the container
-  enableIPv6 = true;
-};
-
-containers.nextcloud = {
-  autoStart = true;
-  privateNetwork = true;
-  hostAddress = "10.22.0.30";
-  localAddress = "10.22.0.31";
-  config = { config, pkgs, lib, ... }: {
-
-    services.nextcloud = {
-      enable = true;
-      package = pkgs.nextcloud28;
-      hostName = "localhost";
-      config.adminpassFile = "${pkgs.writeText "adminpass" "test123"}"; # DON'T DO THIS IN PRODUCTION - the password file will be world-readable in the Nix Store!
     };
-
-    system.stateVersion = "24.11";
-
-    networking = {
-      firewall = {
-        enable = true;
-        allowedTCPPorts = [ 80 ];
-      };
-      # Use systemd-resolved inside the container
-      # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-      useHostResolvConf = lib.mkForce false;
-    };
-    
-    services.resolved.enable = true;
-
-  };
-};
   
   };  
 }
