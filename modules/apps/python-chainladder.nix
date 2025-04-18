5c94348beb573ca40c8599985c498ea3cd2d4441
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-18 14:16:37 +0000

    Update python-chainladder.nix

--- modules/apps/python-chainladder.nix
+++ modules/apps/python-chainladder.nix
@@ -29,7 +29,7 @@
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     environment.systemPackages = with pkgs; [
-      (builtins.getFlake "github:matleborgne/chainladder/17d8e21ca957f925ee19571a895ffdc307d68f74").packages.x86_64-linux.default
+      (builtins.getFlake "github:matleborgne/chainladder/266d0b328918cbaf2ebb52d567c50a7a874926e6").packages.x86_64-linux.default
     ];
 
   };
