bbf01092318e15b030b601a29f46e6cbc8a3c9b6
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 10:56:01 +0000

    Create suspend-fix.nix

---
+++ modules/desktop/gnome/suspend-fix.nix
@@ -0,0 +1,61 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  options.mle.desktop.gnome.suspend-fix.enable = lib.mkOption {
+    description = "GNOME suspend-fix";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.desktop.gnome.suspend-fix.enable {
+		
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Module
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    systemd = {
+    
+      services."gnome-suspend" = {
+        description = "suspend gnome shell";
+        before = [
+          "systemd-suspend.service"
+          "systemd-hibernate.service"
+          "nvidia-suspend.service"
+          "nvidia-hibernate.service"
+        ];
+        wantedBy = [
+          "systemd-suspend.service"
+          "systemd-hibernate.service"
+        ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
+        };
+      };
+    
+      services."gnome-resume" = {
+        description = "resume gnome shell";
+        after = [
+          "systemd-suspend.service"
+          "systemd-hibernate.service"
+          "nvidia-resume.service"
+        ];
+        wantedBy = [
+          "systemd-suspend.service"
+          "systemd-hibernate.service"
+        ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
+        };
+      };
+    };
+
+	};
+}
