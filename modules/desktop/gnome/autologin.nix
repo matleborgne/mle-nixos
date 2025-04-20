3912db393e053188821c4ca5af668ad60b54b28c
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 11:27:39 +0000

    Update autologin.nix

--- modules/desktop/gnome/autologin.nix
+++ modules/desktop/gnome/autologin.nix
@@ -1,1 +1,28 @@
+{ config, lib, pkgs, ... }:
 
+{
+
+  options.mle.desktop.gnome.autologin.enable = lib.mkOption {
+    description = "GNOME autologin";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.desktop.gnome.autologin.enable {
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Module
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    services.xserver.displayManager.gdm.autoLogin = {
+      enable = true;
+      inherit user;
+    };
+
+  };
+}
