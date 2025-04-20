66a0c63932d8919825f56f4e181fd1e1896480fb
Mathieu <mathieu.leborgne.pro@pm.me>
2025-04-20 13:44:09 +0200

    Update autologin.nix

--- modules/desktop/gnome/autologin.nix
+++ modules/desktop/gnome/autologin.nix
@@ -14,7 +14,7 @@
   let
     allUsers = builtins.attrNames config.users.users;
     normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
-    user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
+    mainUser = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
 
   in {
 
@@ -29,7 +29,7 @@
 
     services.xserver.displayManager.gdm.autoLogin = {
       enable = true;
-      inherit user;
+      user = "$mainUser";
     };
 
   });
