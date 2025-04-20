67a25c2bcf2c22ffbb275b83925960310c7a5860
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 23:45:47 +0000

    Update nspawn-nextcloud.nix

--- modules/containers/nspawn-nextcloud.nix
+++ modules/containers/nspawn-nextcloud.nix
@@ -57,7 +57,7 @@
 
           # Customisation for container
           services.nextcloud = {
-            settings = { trusted_domains = address; };
+            settings = { trusted_domains = [ builtins.elemAt (builtins.split "/" (builtins.elemAt address 0)) 0 ]; };
             config = { adminpassFile = "/ncpassfile"; };
           };
 
