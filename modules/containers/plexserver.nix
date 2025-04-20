2ada46294f7ecaeb1ac54be1299d3051c3ec90ae
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 00:01:19 +0000

    Update plexserver.nix

--- modules/containers/plexserver.nix
+++ modules/containers/plexserver.nix
@@ -25,8 +25,8 @@
     };
 
     privateNetwork = true;
-    hostAddress = "10.22.0.154";  # change this
-    localAddress = "10.22.0.155"; # change this, NOT THE SAME AS HOST ADDRESS
+    hostAddress = "10.22.0.174";  # change this
+    localAddress = "10.22.0.175"; # change this, NOT THE SAME AS HOST ADDRESS
 
     config = { lib, config, pkgs, options, ... }: {
       systemd.tmpfiles.rules = [ "d /var/lib/plex 700 plex plex -" ];
