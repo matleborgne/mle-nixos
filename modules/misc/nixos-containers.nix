1bc82af3b811ba7161481b93c0b36b064d8eb433
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 12:49:55 +0000

    Update nixos-containers.nix

--- modules/misc/nixos-containers.nix
+++ modules/misc/nixos-containers.nix
@@ -31,21 +31,31 @@
 # TODO test if needed for nspawn systemd nixos-containers
     virtualisation =  {
       containers.enable = true;
+      vlans = [ 1 ];
     };
 
+    networking.macvlans.mv-eth1-host = {
+      interface = "eth1";
+      mode = "bridge";
+    };
+
+    networking.interfaces.eth1.ipv4.addresses = lib.mkForce [];
+    networking.interfaces.mv-eth1-host = {
+      ipv4.addresses = [ { address = "10.22.0.1"; prefixLength = 24; } ];
+    };
 
     boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
-    networking.nat = {
-      enable = true;
-      internalInterfaces = ["ve-+"];   # "ve-*" if using nftables instead of iptables
-      externalInterface = "enp3s0";    
-      # TODO read the interface name # ip link | grep enp | awk -F ": " '{ print $2 }'
-    };
+#    networking.nat = {
+#      enable = true;
+#      internalInterfaces = ["ve-+"];   # "ve-*" if using nftables instead of iptables
+#      externalInterface = "enp3s0";    
+#      # TODO read the interface name # ip link | grep enp | awk -F ": " '{ print $2 }'
+#    };
 
-    networking = {
-      useNetworkd = true;
-      useDHCP = true;
+#    networking = {
+#      useNetworkd = true;
+#      useDHCP = true;
 
 #      bridges = {
 #        br0 = { interfaces = [ "enp3s0" ]; };
