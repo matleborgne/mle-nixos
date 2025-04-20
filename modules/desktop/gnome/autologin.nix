51ebe85135c301ecf9ffc70ec254f23eb634f524
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 11:29:08 +0000

    Update autologin.nix

--- modules/desktop/gnome/autologin.nix
+++ modules/desktop/gnome/autologin.nix
@@ -8,8 +8,16 @@
     default = false;
   };
 
-  config = lib.mkIf config.mle.desktop.gnome.autologin.enable {
-    
+  config = lib.mkIf config.mle.desktop.gnome.autologin.enable (
+
+
+  let
+    allUsers = builtins.attrNames config.users.users;
+    normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
+    user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
+
+  in {
+
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Recursive activation of other mle.<modules>
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
