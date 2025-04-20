5b78e8e454a509461066773955c530ac5ff42684
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 00:04:34 +0000

    Update n2.nix

--- roles/n2.nix
+++ roles/n2.nix
@@ -34,21 +34,15 @@
       cockpit.enable = true;
     };
 
-    #docker = {
-    #  portainer.enable = true;
-    #};
-
     misc = {
-      #docker.enable = true;
       libvirt.enable = true;
       #mleupdater.enable = true;
-      #nixos-containers.enable = true;
-      podman.enable = true;
+      nixos-containers.enable = true;
       sshfs.enable = true;
     };
 
-    podman = {
-      portainer.enable = true;
+    nspawn = {
+      plexserver.enable = true;
     };
 
     secrets = {
