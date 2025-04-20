d82f10a522fd7912d81537d239e8524bd04f2c22
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 17:32:24 +0000

    Update nspawn-plexserver.nix

--- modules/containers/nspawn-plexserver.nix
+++ modules/containers/nspawn-plexserver.nix
@@ -33,6 +33,8 @@
         };
 
         config = { lib, config, pkgs, options, ... }: {
+
+          imports = 
       
           # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
           # Main services
