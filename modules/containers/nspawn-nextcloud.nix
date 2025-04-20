35c8df3f9b2493502904dddbe6cba6ba999ffa44
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-20 23:09:48 +0000

    Create nspawn-nextcloud.nix

---
+++ modules/containers/nspawn-nextcloud.nix
@@ -0,0 +1,128 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CONTAINERS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Containers modules are the place for declaration of
+# nixos-containers, through nspawn from systemd
+
+{
+
+  options.mle.nspawn.nextcloud.enable = lib.mkOption {
+    description = "Configure nextcloud nspawn container";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.nspawn.nextcloud.enable (
+
+    let
+      address = [ "10.22.0.153/24" ]; # change this accord to desired local IP
+
+    in {
+
+      containers.nextcloud = {
+        autoStart = true;
+        ephemeral = false;
+        privateNetwork = false;
+        macvlans = [ "enp3s0" ];
+
+        bindMounts = {
+          "/var/lib/nextcloud" = { hostPath = "/var/lib/nspawn/nextcloud/app"; isReadOnly = false; };
+          "/var/lib/postgresql" = { hostPath = "/var/lib/nspawn/nextcloud/db"; isReadOnly = false; };
+          #"/mnt/nfs" = { hostPath = "/var/srv"; isReadOnly = false; };
+          "/passfile" = { hostPath = "/etc/nixos/build/secrets/keys/restic_passfile"; isReadOnly = true; };
+        };
+
+        config = { lib, config, pkgs, options, ... }: {
+
+          imports = [
+            ../apps/nextcloud.nix
+            ../apps/fish.nix
+          ];
+      
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Main services
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          #systemd.tmpfiles.rules = [ "d /var/lib/nextcloud 700 plex plex -" ];
+
+          mle = {
+            apps = {
+              nextcloud.enable = true;
+              fish.enable = true;
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Network
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          system.stateVersion = "24.11";
+
+          networking = {
+            hostName = "nextcloud";
+
+            useNetworkd = true;
+            useDHCP = false;
+            useHostResolvConf = false;
+          };
+
+          systemd.network = {
+            enable = true;
+            networks = {
+              "40-mv-enp3s0" = {
+                matchConfig.Name = "mv-enp3s0";
+                networkConfig.DHCP = "yes";
+                dhcpV4Config.ClientIdentifier = "mac";
+                inherit address;
+              };
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Auto Backup
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          environment.systemPackages = with pkgs; [ restic ];
+
+          services.restic.backups = {
+
+            nextcloud-app = {
+              initialize = false;
+              repository = "/mnt/nfs/bkp/lxc/553-nextcloud/app";
+              paths = [ "/var/lib/nextcloud" ];
+              passwordFile = "/passfile";
+              pruneOpts = [ "--keep-weekly 5" "--keep-monthly 3" ];
+              timerConfig = {
+                OnCalendar = "Wed 05:30";
+                Persistent = "true";
+              };
+            };
+
+            nextcloud-db = {
+              initialize = false;
+              repository = "/mnt/nfs/bkp/lxc/553-nextcloud/db";
+              paths = [ "/var/lib/postgresql" ];
+              passwordFile = "/passfile";
+              pruneOpts = [ "--keep-weekly 5" "--keep-monthly 3" ];
+              timerConfig = {
+                OnCalendar = "Wed 07:30";
+                Persistent = "true";
+              };
+            };
+
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Other services
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        };
+      };
+  });
+}
