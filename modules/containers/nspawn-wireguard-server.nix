b377224d584a4115e84c842ff823ff17afb5e987
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-21 11:22:31 +0000

    Update nspawn-wireguard-server.nix

--- modules/containers/nspawn-wireguard-server.nix
+++ modules/containers/nspawn-wireguard-server.nix
@@ -1,1 +1,92 @@
+{ lib, config, pkgs, ... }:
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CONTAINERS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Containers modules are the place for declaration of
+# nixos-containers, through nspawn from systemd
+
+{
+
+  options.mle.nspawn.wireguard-server.enable = lib.mkOption {
+    description = "Configure wireguard-server nspawn container";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.nspawn.wireguard-server.enable (
+
+    let
+      address = [ "10.22.0.154/24" ]; # change this accord to desired local IP
+
+    in {
+
+      containers.wireguard-server = {
+        autoStart = true;
+        ephemeral = false;
+        privateNetwork = false;
+        macvlans = [ "enp3s0" ];
+
+        #bindMounts = {
+        #  "/var/lib/nextcloud" = { hostPath = "/var/lib/nspawn/nextcloud/app"; isReadOnly = false; };
+        #};
+
+        config = { lib, config, pkgs, options, ... }: {
+
+          imports = [
+            ../apps/wireguard-server.nix
+            ../apps/fish.nix
+            ../../secrets/apps/wireguard-server.nix
+          ];
+      
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Main services
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          mle = {
+            apps = {
+              wireguard-server.enable = true;
+              fish.enable = true;
+            };
+            secrets = {
+              wireguard-server.enable = true;
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Network
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          system.stateVersion = "24.11";
+
+          networking = {
+            hostName = "nextcloud";
+
+            useNetworkd = true;
+            useDHCP = false;
+            useHostResolvConf = false;
+          };
+
+          systemd.network = {
+            enable = true;
+            networks = {
+              "40-mv-enp3s0" = {
+                matchConfig.Name = "mv-enp3s0";
+                networkConfig.DHCP = "yes";
+                dhcpV4Config.ClientIdentifier = "mac";
+                inherit address;
+              };
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Other services
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        };
+      };
+  });
+}
