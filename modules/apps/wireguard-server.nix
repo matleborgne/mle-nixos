539efa46a6d8f69cbc78f6f71ecda02bf8e11c0a
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-21 00:02:26 +0000

    Create wireguard-server.nix

---
+++ modules/apps/wireguard-server.nix
@@ -0,0 +1,44 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# APPS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Apps modules are the place for customization of individual apps
+# This is not the place for bundles, hardware, desktop, etc.
+# Only for individual applications
+
+{
+
+  options.mle.apps.wireguard-server.enable = lib.mkOption {
+    description = "Configure wireguard-server app";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.apps.wireguard-server.enable {
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Activation and customization of APP
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      environment.systemPackages = with pkgs; [
+        wireguard-tools
+      ];
+
+      networking.firewall = {
+        allowedTCPPorts = [];
+        allowedUDPPorts = [];
+      };
+
+      d
+
+    
+  };
+}
