b4e424136caccaa6e9233ea56e770dcdd0e22f9f
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-21 19:05:25 +0000

    Create nspawn-wireguard-quick.nix

---
+++ modules/containers/nspawn-wireguard-quick.nix
@@ -0,0 +1,93 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CONTAINERS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Containers modules are the place for declaration of
+# nixos-containers, through nspawn from systemd
+
+{
+
+  options.mle.nspawn.wireguard-quick.enable = lib.mkOption {
+    description = "Configure wireguard-quick nspawn container";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.nspawn.wireguard-quick.enable (
+
+    let
+      address = [ "10.22.0.154/24" ]; # change this accord to desired local IP
+
+    in {
+
+      containers.wireguard-quick = {
+        autoStart = true;
+        ephemeral = false;
+        privateNetwork = true;
+        macvlans = [ "enp3s0" ];
+
+        bindMounts = {
+          "/etc/nixos/build/secrets/keys/wireguard" = { hostPath = "/etc/nixos/build/secrets/keys/wireguard"; isReadOnly = false; };
+        };
+
+        config = { lib, config, pkgs, options, ... }: {
+
+          imports = [
+            ../apps/wireguard/quick-server.nix
+            ../apps/fish.nix
+          ];
+      
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Main services
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          mle = {
+            apps = {
+              wireguard-quick.enable = true;
+              fish.enable = true;
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Network
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          system.stateVersion = "24.11";
+
+          boot.kernel.sysctl = {
+            "net.ipv4.ip_forward" = 1;
+            "net.ipv6.ip_forward" = 1;
+          };
+
+          networking = {
+            hostName = "wireguard-quick";
+
+            useNetworkd = true;
+            useDHCP = false;
+            useHostResolvConf = false;
+          };
+
+          systemd.network = {
+            enable = true;
+            networks = {
+              "40-mv-enp3s0" = {
+                matchConfig.Name = "mv-enp3s0";
+                networkConfig.DHCP = "yes";
+                dhcpV4Config.ClientIdentifier = "mac";
+                inherit address;
+              };
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Other services
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        };
+      };
+  });
+}
