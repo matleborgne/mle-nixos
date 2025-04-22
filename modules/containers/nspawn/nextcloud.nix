1544755b66a1b4ecb69c8e023896f93d43fe872e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-22 13:46:35 +0000

    Update nextcloud.nix

--- modules/containers/nspawn/nextcloud.nix
+++ modules/containers/nspawn/nextcloud.nix
@@ -35,7 +35,7 @@
           "/ncpassfile" = { hostPath = "/etc/nixos/build/secrets/keys/nextcloud_passfile"; isReadOnly = true; };
         };
 
-        config = { lib, config, pkgs, options, ... }: {
+        config = { lib, config, pkgs, options, mle, ... }: {
 
           imports = [
             ../../apps/nextcloud.nix
