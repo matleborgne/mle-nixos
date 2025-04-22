165f9d51b12f64a193d50a1ac9859880aa8dc25c
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-22 13:32:18 +0000

    Update nspawn-nextcloud.nix

--- roles/nspawn-nextcloud.nix
+++ roles/nspawn-nextcloud.nix
@@ -10,6 +10,10 @@
 # 3- Note it is possible to override config present in some module
 # To do that, use "lib.mkForce"
 
+let
+  address = [ "10.22.0.153/24" ];
+
+in
 {
 
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,7 +23,7 @@
   # Hostname
   networking.hostName = "nextcloud";
 
-  # Testing kernel because of new hardware
+  # Kernel
   boot.kernelPackages = pkgs.linuxPackages;
 
 
@@ -38,15 +42,6 @@
     misc = {
       networkd.enable = true;
     };
-
-    secrets = {
-      hm-mleborgne.enable = true;
-      libvirt-vm103.enable = true;
-      libvirt-vm104.enable = true;
-      libvirt-vm901.enable = true;
-      openssh-server.enable = true;
-      openssh-client-mleborgne.enable = true;
-    };
     
   };
 
@@ -55,11 +50,43 @@
   # 3- Modification of mle.module (through lib.mkForce)
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  # Example with force bluetooth disabled
-  # Bad example because here we disable a full module, so we could do
-  # mle.bluetooth.enable = lib.mkForce false;
-  hardware.bluetooth.enable = lib.mkForce false;
+  services.nextcloud = {
+    # Very sensitive Nextcloud-installer about trusted domain : only IP without mask or anything else
+    settings = { trusted_domains = [ (builtins.elemAt (builtins.split "/" (builtins.elemAt address 0)) 0) ]; };
+    config = { adminpassFile = "/ncpassfile"; };
+  };
 
-  # Works better for example with a gsettings parameter to override (font size, etc.)
+  systemd.network."40-mv-enp3s0" = { inherit address };
+
+  environment.systemPackages = with pkgs; [ restic ];
+
+  services.restic.backups = {
+
+    nextcloud-app = {
+      initialize = false;
+      repository = "/mnt/nfs/bkp/lxc/553-nextcloud/app";
+      paths = [ "/var/lib/nextcloud" ];
+      passwordFile = "/passfile";
+      pruneOpts = [ "--keep-weekly 5" "--keep-monthly 3" ];
+      timerConfig = {
+        OnCalendar = "Wed 05:30";
+        Persistent = "true";
+      };
+    };
+
+    nextcloud-db = {
+      initialize = false;
+      repository = "/mnt/nfs/bkp/lxc/553-nextcloud/db";
+      paths = [ "/var/lib/postgresql" ];
+      passwordFile = "/passfile";
+      pruneOpts = [ "--keep-weekly 5" "--keep-monthly 3" ];
+      timerConfig = {
+        OnCalendar = "Wed 07:30";
+        Persistent = "true";
+      };
+    };
+
+  };
+
 
 }
