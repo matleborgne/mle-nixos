714329f8d6bab658c5e4f2931df0326a9aeade1a
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-23 12:52:30 +0000

    Update wireguard-quick.nix

--- modules/containers/nspawn/wireguard-quick.nix
+++ modules/containers/nspawn/wireguard-quick.nix
@@ -18,6 +18,7 @@
   config = lib.mkIf config.mle.containers.nspawn.wireguard-quick.enable (
 
     let
+      name = "wireguard-quick";
       address = [ "10.22.0.154/24" ]; # change this accord to desired local IP
 
     in {
@@ -33,10 +34,15 @@
         };
 
         config = { lib, config, pkgs, options, ... }: {
+          system.stateVersion = "24.11";
+
+          networking.hostName = name;
+          systemd.network.networks."40-mv-enp3s0" = { inherit address; };
 
           imports = [
             ../../apps/wireguard/quick-server.nix
             ../../apps/fish.nix
+            ../../misc/networkd.nix
           ];
       
           # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -48,6 +54,9 @@
               wireguard.quick-server.enable = true;
               fish.enable = true;
             };
+            misc = {
+              networkd.enable = true;
+            };
           };
 
 
@@ -55,37 +64,12 @@
           # Network
           # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-          system.stateVersion = "24.11";
-
           boot.kernel.sysctl = {
             "net.ipv4.ip_forward" = 1;
             "net.ipv6.ip_forward" = 1;
           };
 
-          networking = {
-            hostName = "wireguard-quick";
 
-            useNetworkd = true;
-            useDHCP = false;
-            useHostResolvConf = false;
-          };
-
-          systemd.network = {
-            enable = true;
-            networks = {
-              "40-mv-enp3s0" = {
-                matchConfig.Name = "mv-enp3s0";
-                networkConfig.DHCP = "yes";
-                dhcpV4Config.ClientIdentifier = "mac";
-                inherit address;
-              };
-            };
-          };
-
-
-          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-          # Other services
-          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         };
       };
