44312de382ca3404e776bc48ec92ab31fb954bd5
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-23 13:36:33 +0000

    Update network-interface.sh

--- scripts/network-interface.sh
+++ scripts/network-interface.sh
@@ -4,5 +4,5 @@ current=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 netIface=$(ip link | grep enp | awk -F ": " '{ print $2 }')
 
 cat <<- EOF > "$current/../secrets/keys/netIface"
-{ ifaceString = \"$netIface\"; ifaceList = [ $netIface ] }
+{ ifaceString = "$netIface"; ifaceList = [ $netIface ] }
 EOF
