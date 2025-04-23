0af158b7d9116ebecf5fbc4e67c968aa0bb52657
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-23 12:35:05 +0000

    Update plexserver.nix

--- modules/containers/nspawn/plexserver.nix
+++ modules/containers/nspawn/plexserver.nix
@@ -54,7 +54,7 @@
 
           system.stateVersion = "24.11";
 
-          networking.hostName = "$name";
+          networking.hostName = name;
           systemd.network.networks."40-mv-enp3s0" = { inherit address; };
 
           systemd.tmpfiles.rules = [ "d /var/lib/plex 700 plex plex -" ];
