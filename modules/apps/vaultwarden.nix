6f128637ba32b3acf182de61a20bb94c0ca221a1
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-23 12:17:59 +0000

    Create vaultwarden.nix

---
+++ modules/apps/vaultwarden.nix
@@ -0,0 +1,65 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# APPS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Apps modules are the place for customization of individual apps
+# This is not the place for bundles, hardware, desktop, etc.
+# Only for individual applications
+
+{
+
+  options.mle.apps.vaultwarden.enable = lib.mkOption {
+    description = "Configure vaultwarden app";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.apps.vaultwarden.enable {
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Activation and customization of APP
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    #nixpkgs.config.allowUnfree = lib.mkForce true;
+
+    services.vaultwarden = {
+      enable = true;
+      package = pkgs.vaultwarden;
+      dbBackend = "sqlite";
+
+      config = {
+        SIGNUPS_ALLOWED = true;
+        DATABASE_URL = "/var/lib/vaultwarden/db.sqlite3";
+        ROCKET_PORT = "8080";
+      };
+    };
+
+    services.nginx = {
+      enable = true;
+  
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+  
+      virtualHosts = {
+        "vaultwarden.example.com" = {
+          locations."/" = {
+            proxyPass = "http://localhost:8080";
+            proxyWebsockets = true;
+          };
+        };
+      };
+    };
+
+
+  };
+}
