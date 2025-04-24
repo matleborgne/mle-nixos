7993f4f2aa76e9f0032c1539a22d2a2a71d4e09a
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-24 10:50:37 +0000

    Create adguardhome.nix

---
+++ modules/containers/nspawn/adguardhome.nix
@@ -0,0 +1,112 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CONTAINERS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Containers modules are the place for declaration of
+# nixos-containers, through nspawn from systemd
+
+{
+
+  options.mle.containers.nspawn.adguardhome.enable = lib.mkOption {
+    description = "Configure adguardhome nspawn container";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.containers.nspawn.adguardhome.enable (
+
+    let
+      name = "adguardhome";
+      net = (import ../../../secrets/keys/netIface);
+      address = [ "10.22.0.157/24" ]; # change this accord to desired local IP
+
+    in {
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Host prerequisites
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      mle.misc.nixos-containers.enable = lib.mkForce true;
+
+      #systemd.tmpfiles.rules = [
+      #  "d /var/lib/adguardhome - - - -"
+      #];
+
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Container structure
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      containers.${name} = {
+
+        autoStart = true;
+        ephemeral = false;
+        privateNetwork = true;
+        macvlans = net.ifaceList;
+
+        #bindMounts = {
+        #  "/var/lib/plex" = { hostPath = "/var/lib/plex"; isReadOnly = false; };
+        #  "/mnt/nfs" = { hostPath = "/var/srv"; isReadOnly = false; };
+        #  "/passfile" = { hostPath = "/etc/nixos/build/secrets/keys/restic_passfile"; isReadOnly = true; };
+        #};
+
+        config = { lib, config, pkgs, options, ... }: {
+          system.stateVersion = "24.11";
+
+          networking.hostName = name;
+          systemd.network.networks."40-mv-${net.iface}" = { inherit address; };
+
+      
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Running services inside the container
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          #systemd.tmpfiles.rules = [ "d /var/lib/plex 700 plex plex -" ];
+
+          imports = [
+            ../../apps/adguardhome.nix
+            ../../apps/fish.nix
+            ../../misc/networkd.nix
+          ];
+
+          mle = {
+            apps = {
+              adguardhome.enable = true;
+              fish.enable = true;
+            };
+            misc = {
+              networkd.enable = true;
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Backup service
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          environment.systemPackages = with pkgs; [ restic ];
+
+#          services.restic.backups = {
+
+#            adguardhome = {
+#              initialize = false;
+#              repository = "/mnt/nfs/bkp/lxc/552-plexserver";
+#              paths = [ "/var/lib/plex" ];
+#              passwordFile = "/passfile";
+#              pruneOpts = [ "--keep-weekly 5" "--keep-monthly 3" ];
+#              timerConfig = {
+#                OnCalendar = "Tue 05:25";
+#                Persistent = "true";
+#              };
+#            };
+
+#          };
+
+
+
+        };
+      };
+  });
+}
