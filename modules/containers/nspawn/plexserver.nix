bd65dba82c84d7fd429d066da55689845053bfba
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-24 23:39:44 +0000

    Update plexserver.nix

--- modules/containers/nspawn/plexserver.nix
+++ modules/containers/nspawn/plexserver.nix
@@ -20,7 +20,7 @@
     let
       name = "plexserver";
       net = (import ../../../secrets/keys/netIface);
-      address = [ "10.22.0.152/24" ]; # change this accord to desired local IP
+      address = (import ../../../secrets/containers_ips).plexserver;
 
     in {
 
