958ff7f1ff098ee20815e2c112aca5055b1c371e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-24 23:18:36 +0000

    Create mdadm.nix

---
+++ modules/misc/mdadm.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MISC
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Misc are configuration very specific which does not enter
+# in apps, bundles, desktop or hardware categories
+
+{
+
+  options.mle.misc.mdadm.enable = lib.mkOption {
+    description = "Enable mdadm service";
+    type = lib.types.bool;
+    default = false;
+  };
+  
+  config = lib.mkIf config.mle.misc.mdadm.enable {
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Misc configuration
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    boot.swraid = {
+      enable = true;
+      mdadmConf = (builtins.readFile ../../secrets/mdadm.conf);
+    };
+
+    # Override mdmonitor to log to syslog instead of emailing or alerting
+    systemd.services."mdmonitor".environment = {
+      MDADM_MONITOR_ARGS = "--scan --syslog";
+    };
+    
+
+  };
+}
