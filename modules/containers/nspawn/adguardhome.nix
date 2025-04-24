137035b4d6dd01f7c64a2ca30017af13e3215fb6
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-24 11:36:31 +0000

    Update adguardhome.nix

--- modules/containers/nspawn/adguardhome.nix
+++ modules/containers/nspawn/adguardhome.nix
@@ -66,11 +66,6 @@
 
           networking.firewall.enable = lib.mkForce false;
 
-          boot.kernel.sysctl = {
-            "net.ipv4.ip_forward" = 1;
-            "net.ipv6.ip_forward" = 1;
-          };
-
           imports = [
             ../../apps/adguardhome.nix
             ../../apps/fish.nix
