af18362f6624878420340490be0e08fd3696acd5
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-24 10:32:16 +0000

    Create adguardhome.nix

---
+++ modules/apps/adguardhome.nix
@@ -0,0 +1,50 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# APPS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Apps modules are the place for customization of individual apps
+# This is not the place for bundles, hardware, desktop, etc.
+# Only for individual applications
+
+{
+
+  options.mle.apps.adguardhome.enable = lib.mkOption {
+    description = "Configure adguardhome app";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.apps.adguardhome.enable {
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Activation and customization of APP
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    #nixpkgs.config.allowUnfree = lib.mkForce true;
+
+    services.adguardhome = {
+      enable = true;
+      package = lib.mkDefault pkgs.adguardhome;
+      openFirewall = lib.mkDefault true;
+
+      # Config made on web interface overwrite the declarative config
+      mutableSettings = lib.mkDefault false;
+
+
+
+
+    };
+
+ 
+
+
+  };
+}
