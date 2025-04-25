df6799a5f0d96de630d8d04810fbf4ce26177be7
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-25 10:44:36 +0000

    Update vaultwarden.nix

--- modules/apps/vaultwarden.nix
+++ modules/apps/vaultwarden.nix
@@ -49,45 +49,45 @@
       };
     };
 
-    services.nginx = {
-      enable = true;
-  
-      recommendedGzipSettings = true;
-      recommendedOptimisation = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
-  
-      virtualHosts = {
-        "vaultwarden.example.com" = {
-          http2 = true;
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:80";
-            proxyWebsockets = true;
-          };
-
-          extraConfig = ''
-  
-            # Forward real ip and host to Plex
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Host $server_addr;
-            proxy_set_header Referer $server_addr;
-            proxy_set_header Origin $server_addr;
-  
-            # Websockets
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
-  
-            # Buffering off send to the client as soon as the data is received from Plex.
-            proxy_redirect off;
-            proxy_buffering off;
-          '';
-
-        };
-      };
-    };
+#    services.nginx = {
+#      enable = true;
+#  
+#      recommendedGzipSettings = true;
+#      recommendedOptimisation = true;
+#      recommendedProxySettings = true;
+#      recommendedTlsSettings = true;
+#  
+#      virtualHosts = {
+#        "vaultwarden.example.com" = {
+#          http2 = true;
+#          locations."/" = {
+#            proxyPass = "http://127.0.0.1:80";
+#            proxyWebsockets = true;
+#          };
+#
+#          extraConfig = ''
+#  
+#            # Forward real ip and host to Plex
+#            proxy_set_header X-Real-IP $remote_addr;
+#            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#            proxy_set_header X-Forwarded-Proto $scheme;
+#            proxy_set_header Host $server_addr;
+#            proxy_set_header Referer $server_addr;
+#            proxy_set_header Origin $server_addr;
+#  
+#            # Websockets
+#            proxy_http_version 1.1;
+#            proxy_set_header Upgrade $http_upgrade;
+#            proxy_set_header Connection "upgrade";
+#  
+#            # Buffering off send to the client as soon as the data is received from Plex.
+#            proxy_redirect off;
+#            proxy_buffering off;
+#          '';
+#
+#        };
+#      };
+#    };
 
 
   });
