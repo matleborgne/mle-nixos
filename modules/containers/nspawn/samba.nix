6c805d88717c57c89f04105674ab33d4d6617df9
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-25 23:12:33 +0000

    Create samba.nix

---
+++ modules/containers/nspawn/samba.nix
@@ -0,0 +1,94 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CONTAINERS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Containers modules are the place for declaration of
+# nixos-containers, through nspawn from systemd
+
+{
+
+  options.mle.containers.nspawn.samba.enable = lib.mkOption {
+    description = "Configure samba nspawn container";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.containers.nspawn.samba.enable (
+
+    let
+      name = "samba";
+      net = (import ../../../secrets/keys/netIface);
+      address = (import ../../../secrets/containers_ips).samba;
+
+    in {
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Host prerequisites
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      mle.misc.nixos-containers.enable = lib.mkForce true;
+
+      systemd.tmpfiles.rules = [
+        "d /srv - - - -"
+      ];
+
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Container structure
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      containers.${name} = {
+
+        autoStart = true;
+        ephemeral = false;
+        privateNetwork = true;
+        macvlans = net.ifaceList;
+
+        #bindMounts = {
+        #  "/var/lib/plex" = { hostPath = "/var/lib/plex"; isReadOnly = false; };
+        #};
+
+        config = { lib, config, pkgs, options, ... }: {
+          system.stateVersion = "24.11";
+
+          networking.hostName = name;
+          systemd.network.networks."40-mv-${net.iface}" = { inherit address; };
+
+      
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Running services inside the container
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          systemd.tmpfiles.rules = [
+            "d /srv 700 - - -"
+          ];
+
+          imports = [
+            ../../apps/fish.nix
+            ../../misc/networkd.nix
+            ../../misc/samba.nix
+          ];
+
+          mle = {
+            apps = {
+              fish.enable = true;
+            };
+            misc = {
+              networkd.enable = true;
+              samba.enable = true;
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Backup service
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+        };
+      };
+  });
+}
