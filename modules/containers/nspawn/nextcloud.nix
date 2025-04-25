2236671fc28acfcc5c5ff6928c152125d1889969
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-25 11:37:54 +0000

    Update nextcloud.nix

--- modules/containers/nspawn/nextcloud.nix
+++ modules/containers/nspawn/nextcloud.nix
@@ -130,7 +130,7 @@
               passwordFile = "/passfile";
               pruneOpts = [ "--keep-weekly 5" "--keep-monthly 3" ];
               timerConfig = {
-                OnCalendar = "Wed 07:30";
+                OnCalendar = "Wed 07:35";
                 Persistent = "true";
               };
             };
