f461f674ff43a41574519e17b2605a6960f3a4e9
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-26 19:30:21 +0000

    Update sshfs.nix

--- modules/containers/nspawn/sshfs.nix
+++ modules/containers/nspawn/sshfs.nix
@@ -50,7 +50,43 @@
         privateNetwork = true;
         macvlans = net.ifaceList;
 
-        bindMounts = mleborgne.ctMounts // pbachelier.ctMounts;
+
+        ExtensionSettings = with builtins;
+
+          let extension = shortId: uuid: {
+            name = uuid;
+            value = {
+              install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+              installation_mode = "normal_installed";
+            };
+          };
+          
+          in listToAttrs [
+            (extension "ublock-origin" "uBlock0@raymondhill.net")
+            (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+            (extension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
+            (extension "istilldontcareaboutcookies" "idcac-pub@guus.ninja")
+            (extension "return-youtube-dislikes" "{762f9885-5a13-4abd-9c77-433dcd38b8fd}")
+          ];
+
+      };
+
+
+        # Merging of many attributes sets with redundancy
+        bindMounts = with builtins;
+
+          let bind = path: hostPath: isReadOnly: {
+            name = path;
+            value = {
+              inherit hostPath;
+              inherit isReadOnly;
+            };
+          };
+
+          in listToAttrs [
+
+          ];
+
 
         config = { lib, config, pkgs, options, ... }: {
           system.stateVersion = "24.11";
