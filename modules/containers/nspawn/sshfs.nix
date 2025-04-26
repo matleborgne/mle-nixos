80ddddeab96609b1ef9c579bed2db8979fd53a81
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-26 12:06:21 +0000

    Update sshfs.nix

--- modules/containers/nspawn/sshfs.nix
+++ modules/containers/nspawn/sshfs.nix
@@ -105,7 +105,7 @@
 
           users.users.mleborgne = {
             openssh.authorizedKeys.keys = [ pubkeys.mleborgne ];
-            shell = lib.mkForce "/run/current-system/sw/bin/nologin";
+            #shell = lib.mkForce "/run/current-system/sw/bin/nologin";
             password = pwd.mleborgne;
           };
 
