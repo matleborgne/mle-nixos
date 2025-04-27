156ae409fc7a433546a96f18ddaf7e0d0f8a64bd
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-27 11:53:16 +0000

    Create qbittorrent.nix

---
+++ modules/apps/qbittorrent.nix
@@ -0,0 +1,90 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# APPS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Apps modules are the place for customization of individual apps
+# This is not the place for bundles, hardware, desktop, etc.
+# Only for individual applications
+
+{
+
+  options.mle.apps.qbittorrent.enable = lib.mkOption {
+    description = "Configure qbittorrent app";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.apps.qbittorrent.enable {
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Activation and customization of APP
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    networking.firewall = {
+      allowedTCPPorts = [ 80 8080 ];
+    };
+
+
+    services.qbittorrent-nox = {
+      enable = true;
+      package = pkgs.nextcloud31;
+      hostName = "localhost";
+      settings = lib.mkDefault { trusted_domains = []; };
+
+      config = {
+        dbtype = "pgsql";
+        dbname = "nextcloud";
+        dbuser = "nextcloud";
+        dbhost = "/run/postgresql";
+        adminpassFile = lib.mkDefault "/etc/nextcloud-admin-passfile";
+      };
+    };
+
+
+    services.postgresql = {
+      enable = true;
+      ensureDatabases = [ "nextcloud" ];
+      ensureUsers = [{
+        name = "nextcloud";
+        ensureDBOwnership = true;
+      }];
+    };
+
+    services.postgresqlBackup = {
+      enable = true;
+      location = "/var/lib/postgresql/backup";
+      databases = [ "nextcloud" ];
+      startAt = "*-*-* 03:15:00";
+    };
+
+
+    systemd.services = {
+
+      "nextcloud-setup" = {
+        requires = [ "postgresql.service" ];
+        after = [ "postgresql.service" ];
+      };
+
+      "disable-root" = {
+        enable = true;
+        wantedBy = [ "default.target" ];
+        requires = [ "nextcloud-setup.service" ];
+        after = [ "nextcloud-setup.service" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = ''/run/current-system/sw/bin/nextcloud-occ user:disable root'';
+        };
+      };
+
+    };
+
+
+  };
+}
