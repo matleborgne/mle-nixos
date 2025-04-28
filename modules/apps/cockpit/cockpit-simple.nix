44a34c656e1bcd3a1dc7ef397c30e1b2b7ef1021
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-28 23:37:23 +0000

    Update cockpit-simple.nix

--- modules/apps/cockpit/cockpit-simple.nix
+++ modules/apps/cockpit/cockpit-simple.nix
@@ -33,11 +33,6 @@
       pcp cockpit
     ];
 
-    networking.firewall = {
-      allowedTCPPorts = [ 9090 ];
-      allowedUDPPorts = [ 9090 ];
-    };
-
     services.cockpit = {
       enable = true;
       openFirewall = true;
