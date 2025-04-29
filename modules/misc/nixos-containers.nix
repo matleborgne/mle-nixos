8ae358f55d24a5ed87b403d9eea79b1d90cb5694
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-29 09:29:23 +0000

    Update nixos-containers.nix

--- modules/misc/nixos-containers.nix
+++ modules/misc/nixos-containers.nix
@@ -52,40 +52,5 @@
       ];
     };
 
-
-#    networking.macvlans.mv-eth1-host = {
-#      interface = "eth1";
-#      mode = "bridge";
-#    };
-
-#    networking.interfaces.eth1.ipv4.addresses = lib.mkForce [];
-#    networking.interfaces.mv-eth1-host = {
-#      ipv4.addresses = [ { address = "10.22.0.1"; prefixLength = 24; } ];
-#    };
-
-#    boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
-
-#    networking.nat = {
-#      enable = true;
-#      internalInterfaces = ["ve-+"];   # "ve-*" if using nftables instead of iptables
-#      externalInterface = "enp3s0";    
-#      # TODO read the interface name # ip link | grep enp | awk -F ": " '{ print $2 }'
-#    };
-
-#    networking = {
-#      useNetworkd = true;
-#      useDHCP = true;
-
-#      bridges = {
-#        br0 = { interfaces = [ "enp3s0" ]; };
-#      };
-      
-#      interfaces.br0 = {
-#        useDHCP = true;
-#        ipv4.addresses = [{ address = "10.23.0.3"; prefixLength = 8; }];
-#      };
-
-#    };
-
   };  
 }
