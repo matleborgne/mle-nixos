e053c0f5e658e242887ed767b929f7152c3c855e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-30 00:10:50 +0000

    Update scrutiny.nix

--- modules/apps/scrutiny.nix
+++ modules/apps/scrutiny.nix
@@ -27,16 +27,48 @@
     # Activation and customization of APP
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#    networking.firewall = {
-#      allowedTCPPorts = [ 80 8080 ];
-#    };
+    networking.firewall = {
+      allowedTCPPorts = [ 80 8080 ];
+    };
 
     services.scrutiny = {
       enable = true;
       openFirewall = true;
     };
 
+    services.nginx = {
+      enable = true;
+  
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+  
+      virtualHosts = {
+        "scrutiny.example.com" = {
+          http2 = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:8080/";
+            proxyWebsockets = true;
+          };
+  
+#          extraConfig = ''
+#  
+#            # Avoid disconnect after long pause
+#            send_timeout 100m;
+#  
+#            # Forward real ip and host to Plex
+#            proxy_set_header X-Real-IP $remote_addr;
+#            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#            proxy_set_header X-Forwarded-Proto $scheme;
+#            proxy_set_header Host $server_addr;
+#            proxy_set_header Referer $server_addr;
+#            proxy_set_header Origin $server_addr;
+#          '';
 
+        };
+      };
+    };  
 
   };
 }
