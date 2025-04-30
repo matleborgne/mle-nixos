83a16bcf9389405fc0acf42c9943492d9ec0306c
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-30 00:05:23 +0000

    Create scrutiny.nix

---
+++ modules/containers/nspawn/scrutiny.nix
@@ -0,0 +1,89 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CONTAINERS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Containers modules are the place for declaration of
+# nixos-containers, through nspawn from systemd
+
+{
+
+  options.mle.containers.nspawn.scrutiny.enable = lib.mkOption {
+    description = "Configure scrutiny nspawn container";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.containers.nspawn.scrutiny.enable (
+
+    let
+      name = "scrutiny";
+      net = (import ../../../secrets/keys/netIface);
+      address = (import ../../../secrets/containers_ips).scrutiny;
+
+    in {
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Host prerequisites
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      mle.misc.nixos-containers.enable = lib.mkForce true;
+
+      systemd.tmpfiles.rules = [
+        "d /var/lib/scrutiny - - - -"
+        "d /srv - - - -"
+      ];
+
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Container structure
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      containers.${name} = {
+
+        autoStart = true;
+        ephemeral = false;
+        privateNetwork = true;
+        macvlans = net.ifaceList;
+
+        bindMounts = {
+          "/var/lib/scrutiny" = { hostPath = "/var/lib/scrutiny"; isReadOnly = false; };
+        };
+
+        config = { lib, config, pkgs, options, ... }: {
+          system.stateVersion = "24.11";
+
+          networking.hostName = name;
+          systemd.network.networks."40-mv-${net.iface}" = { inherit address; };
+
+      
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Running services inside the container
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          systemd.tmpfiles.rules = [
+            "d /var/lib/scrutiny - - - -"
+          ];
+
+          imports = [
+            ../../apps/scrutiny.nix
+            ../../apps/fish.nix
+            ../../misc/networkd.nix
+          ];
+
+          mle = {
+            apps = {
+              scrutiny.enable = true;
+              fish.enable = true;
+            };
+            misc = {
+              networkd.enable = true;
+            };
+          };
+
+
+        };
+      };
+  });
+}
