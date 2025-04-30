e0415003e0afb1a2afe52e71f1dd3a114a696e3e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-04-30 10:58:34 +0000

    Update scrutiny.nix

--- modules/containers/nspawn/scrutiny.nix
+++ modules/containers/nspawn/scrutiny.nix
@@ -43,7 +43,7 @@
         macvlans = net.ifaceList;
 
         bindMounts = {
-          "/scrutiny-collector" = { hostPath = "../../../scripts/scrutiny-collector-metrics-linux-amd64"; isReadOnly = false; };
+          "/scrutiny-collector" = { hostPath = "/etc/nixos/build/scripts/scrutiny-collector-metrics-linux-amd64"; isReadOnly = false; };
           "/dev/sda" = { hostPath = "/dev/sda"; isReadOnly = true; };
           "/dev/sdb" = { hostPath = "/dev/sdb"; isReadOnly = true; };
           "/dev/sdc" = { hostPath = "/dev/sdc"; isReadOnly = true; };
