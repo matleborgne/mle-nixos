d6030062e7967a705cf6e30ef52f7543ac9b0362
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-02 10:15:38 +0000

    Create chainladder.nix

---
+++ modules/apps/python/chainladder.nix
@@ -0,0 +1,66 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# APPS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Apps modules are the place for customization of individual apps
+# This is not the place for bundles, hardware, desktop, etc.
+# Only for individual applications
+
+{
+
+  options.mle.apps.python.chainladder.enable = lib.mkOption {
+    description = "Configure PYTHON CHAINLADDER library";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.apps.python.chainladder.enable {
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Activation and customization of APP
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    let
+      chainladder = pkgs.python3Packages.buildPythonPackage rec {
+        pname = "chainladder";
+        version = "0.8.24";
+        pyproject = true;
+
+        src = pkgs.python3Packages.fetchPypi {
+          inherit pname version;
+          hash = "sha256-z4Q23FnYaVNG/NOrKW3kZCXsqwDWQJbOvnn7Ueyy65M=";
+        };
+
+        build-system = with pkgs.python3Packages; [
+          setuptools
+          setuptools-scm
+        ];
+
+        dependencies = with pkgs.python3Packages; [
+          attrs
+          py
+          setuptools
+          six
+          pluggy
+        ];
+
+        nativeCheckInputs = with pkgs.python3Packages; [
+          hypothesis
+        ];
+      };
+
+    in  
+      environment.systemPackages = with pkgs; [
+        chainladder
+      ];
+
+  };
+}
