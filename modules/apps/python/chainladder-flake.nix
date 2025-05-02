5c1d089787918c8c6374af5a59c3f3625f91e888
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-02 23:36:32 +0000

    Update chainladder-flake.nix

--- modules/apps/python/chainladder-flake.nix
+++ modules/apps/python/chainladder-flake.nix
@@ -29,13 +29,13 @@
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     #environment.systemPackages = with pkgs; [
-    #  (builtins.getFlake "github:matleborgne/chainladder-python/8d23aee410f1e1965697b54b911f73e1f463437c").packages.x86_64-linux.default
+    #  (builtins.getFlake "github:matleborgne/chainladder-python/59daca6ce5d49b180dae93d5c167ab923d4f37a3").packages.x86_64-linux.default
     #];
 
     environment.systemPackages =
 
       let
-        chainladderFlake = builtins.getFlake "github:matleborgne/chainladder-python/8d23aee410f1e1965697b54b911f73e1f463437c";
+        chainladderFlake = builtins.getFlake "github:matleborgne/chainladder-python/59daca6ce5d49b180dae93d5c167ab923d4f37a3";
         
         python = pkgs.python3.override {
           self = python;
