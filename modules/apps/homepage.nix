1764cff5842ac2c73c4158d9836ab8cd782914ae
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-06 15:11:12 +0000

    Update homepage.nix

--- modules/apps/homepage.nix
+++ modules/apps/homepage.nix
@@ -30,6 +30,7 @@
 
     services.homepage-dashboard = {
       enable = true;
+      environmentFile = "/etc/homepage.env";
       widgets = [
         {
           resources = {
@@ -47,86 +48,18 @@
       ];
     };
 
-    system.activationScripts.homeSession = ''
-      export HOMEPAGE_ALLOWED_HOSTS="10.22.0.2"
-    '';
 
-    environment.sessionVariables = {
-      HOMEPAGE_ALLOWED_HOSTS = "10.22.0.2";
+    environment.etc."homepage.env" = {
+      enable = true;
+      text = ''
+        HOMEPAGE_VAR_HOMEPAGE_ALLOWED_HOSTS="10.22.0.2"
+      '';
     };
-    environment.variables = {
-      HOMEPAGE_ALLOWED_HOSTS = "10.22.0.2";
-    };
+
+
     networking.firewall = {
       allowedTCPPorts = [ 8082 ];
     };
-
-    services.nginx = {
-      enable = true;
-  
-      recommendedGzipSettings = true;
-      recommendedOptimisation = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
-  
-      virtualHosts = {
-        "plexserver.example.com" = {
-          http2 = true;
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:32400/";
-            proxyWebsockets = true;
-          };
-  
-          extraConfig = ''
-  
-            # Avoid disconnect after long pause
-            send_timeout 100m;
-  
-            # Forward real ip and host to Plex
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header Host $server_addr;
-            proxy_set_header Referer $server_addr;
-            proxy_set_header Origin $server_addr;
-  
-            # Plex has A LOT of javascript, xml and html. This helps a lot, but if it causes playback issues with devices turn it off.
-            gzip on;
-            gzip_vary on;
-            gzip_min_length 1000;
-            gzip_proxied any;
-            gzip_types text/plain text/css text/xml application/xml text/javascript application/x-javascript image/svg+xml;
-            gzip_disable "MSIE [1-6]\.";
-  
-            # Nginx default client_max_body_size is 1MB, which breaks Camera Upload feature from the phones.
-            client_max_body_size 100M;
-  
-            # Plex headers
-            proxy_set_header X-Plex-Client-Identifier $http_x_plex_client_identifier;
-            proxy_set_header X-Plex-Device $http_x_plex_device;
-            proxy_set_header X-Plex-Device-Name $http_x_plex_device_name;
-            proxy_set_header X-Plex-Platform $http_x_plex_platform;
-            proxy_set_header X-Plex-Platform-Version $http_x_plex_platform_version;
-            proxy_set_header X-Plex-Product $http_x_plex_product;
-            proxy_set_header X-Plex-Token $http_x_plex_token;
-            proxy_set_header X-Plex-Version $http_x_plex_version;
-            proxy_set_header X-Plex-Nocache $http_x_plex_nocache;
-            proxy_set_header X-Plex-Provides $http_x_plex_provides;
-            proxy_set_header X-Plex-Device-Vendor $http_x_plex_device_vendor;
-            proxy_set_header X-Plex-Model $http_x_plex_model;
-  
-            # Websockets
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
-  
-            # Buffering off send to the client as soon as the data is received from Plex.
-            proxy_redirect off;
-            proxy_buffering off;
-          '';
-        };
-      };
-    };   
 
 
   };
