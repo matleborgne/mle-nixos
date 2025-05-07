06896b7b209d09482e7dd96f5d179b295dcb1645
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-07 15:20:13 +0000

    Update vscode.nix

--- modules/flatpaks/vscode.nix
+++ modules/flatpaks/vscode.nix
@@ -57,14 +57,17 @@
     '';
 
     preferences = ''
-      XbracketsOpenX
+      #!/bin/bash
+      cat <<- EOF > "~/.var/app/com.vscodium.codium/config/VSCodium/User/settings.json"
+      {
         "workbench.colorTheme": "GitHub Light Default",
         "workbench.statusBar.visible": false,
         "workbench.editor.enablePreview": false,
         "workbench.startupEditor": "none",
         "editor.minimap.enabled": false,
         "window.zoomLevel": 1.3
-      XbracketsCloseX
+      }
+      EOF
     '';
 
   in {
@@ -87,7 +90,11 @@
       script = ''
         flatpak install --or-update --noninteractive com.vscodium.codium
 
-        echo ${preferences} > ~/.var/app/com.vscodium.codium/config/VSCodium/User/settings.json
+        cat << EOF | flatpak run --command=/bin/bash com.vscodium.codium
+          echo ${preferences} > /var/data/vscode-preferences.sh
+          chmod 755 /var/data/vscode-preferences.sh
+        EOF
+        flatpak run --command=/var/data/vscode-preferences.sh com.vscodium.codium
 
         cat << EOF | flatpak run --command=/bin/bash com.vscodium.codium
           echo ${pythonDeps} > /var/data/vscode-pythonDeps.sh
