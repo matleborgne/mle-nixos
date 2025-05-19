1076355df1e8d797dbc14c5e9a3e3cbaf215fd6c
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-19 22:01:05 +0000

    Update fluent-gtk-theme.nix

--- modules/apps/fluent-gtk-theme.nix
+++ modules/apps/fluent-gtk-theme.nix
@@ -32,14 +32,14 @@
     #  (builtins.getFlake "github:matleborgne/Fluent-gtk-theme/ac0794dcf9e5847fab9263a3b6123e3438863dfa").packages.x86_64-linux.default
     #];
 
-    environment.systemPackages = with pkgs; [(
-      fluent-gtk-theme.override {
+    environment.systemPackages = with pkgs; [
+      (fluent-gtk-theme.override {
         themeVariants = [ "default" ];
         colorVariants = [ "dark" ];
         sizeVariants = [ "standard" ];
         tweaks = [ "solid" "square" "round" ];
-      };
-    )];
+      })
+    ];
 
   };
 
