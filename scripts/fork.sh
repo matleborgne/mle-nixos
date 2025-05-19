e744e7753f85ce7a68b7aa303d8350c224f2486c
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-19 22:19:54 +0000

    Update fork.sh

--- scripts/fork.sh
+++ scripts/fork.sh
@@ -21,22 +21,9 @@ current=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 fork() {
   source=${1}
   if [ -z ${2} ]; then target=${1}; else target=${2}; fi 
-  targetPath="$current/../pkgs/$target.nix"
+  targetPath="${current}/../pkgs/${target}.nix"
 
   wget "$github/$source.nix" -O "$targetPath"
-}
-
-
-replace() {
-  source=${1}
-  target=${2}
-  targetPath="$current/../pkgs/$target.nix"
-
-  sed -i "s/$source/$target/g" "$targetPath"
-  
-  source="$(tr '[:lower:]' '[:upper:]' <<< ${source:0:1})${source:1}"
-  target="$(tr '[:lower:]' '[:upper:]' <<< ${target:0:1})${target:1}"
-  sed -i "s/$source/$target/g" "$targetPath"
 }
   
 
@@ -46,7 +33,9 @@ replace() {
 
 # Librewolf
 fork firefox librewolf
-replace firefox librewolf
+sed -i "s/firefox/librewolf/g" "$current/../pkgs/librewolf.nix"
+sed -i "s/Firefox/Librewolf/g" "$current/../pkgs/librewolf.nix"
 
 # Fluent-gtk-theme
 fork fluent-gtk-theme
+sed -i "s/icon nixos/icon default/g" "$current/../pkgs/fluent-gtk-theme.nix"
