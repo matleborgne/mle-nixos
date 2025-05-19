68222351c29a24c6f8341522f3fa2b5f162d240b
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-19 23:07:50 +0000

    Update chainladder.nix

--- pkgs/chainladder.nix
+++ pkgs/chainladder.nix
@@ -1,59 +1,59 @@
-{ lib, python3Packages, fetchPypi }:
+{
+  lib,
+  python3Packages,
+  fetchPypi,
+  ...
+}:
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# PKGS - PACKAGES
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let
+  cfg = config.programs.chainladder;
 
-# Packages are FUNCTIONS, not MODULES
-# They should never been imported as a module (with import function)
-# or it could be an rebuild-error
+in
+{
+  options.programs.chainladder = {
 
-# To use a package, use something like
-#let
-#  myPackage = pkgs.callPackage /etc/nixos/build/pkgs/myPackage.nix {};
-#in
-#  environment.systemPackage = [ myPackage ] or mkShell or whatever
+    python3Packages.buildPythonPackage rec {
+      pname = "chainladder";
+      version = "0.8.24";
+      pyproject = true;
+
+      src = fetchPypi {
+        inherit pname version;
+        hash = "sha256-RKoDlqQRzYBl8gaiM1VF5sjPJVRWrsGseuefAMm/ojk=";
+      };
+
+      build-system = with python3Packages; [
+        setuptools
+        setuptools-scm
+      ];
+
+      dependencies = with python3Packages; [
+        setuptools
+        scikit-learn
+        matplotlib
+        sparse
+        pandas
+        dill
+        patsy
+        packaging
+      ];
 
 
-python3Packages.buildPythonPackage rec {
-  pname = "chainladder";
-  version = "0.8.24";
-  pyproject = true;
+      propagatedBuildInputs = with python3Packages; [
+        setuptools
+        scikit-learn
+        matplotlib
+        sparse
+        pandas
+        dill
+        patsy
+        packaging
+      ];
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-RKoDlqQRzYBl8gaiM1VF5sjPJVRWrsGseuefAMm/ojk=";
+      nativeCheckInputs = with python3Packages; [
+        hypothesis
+      ];
+    }
+
   };
-
-  build-system = with python3Packages; [
-    setuptools
-    setuptools-scm
-  ];
-
-  dependencies = with python3Packages; [
-    setuptools
-    scikit-learn
-    matplotlib
-    sparse
-    pandas
-    dill
-    patsy
-    packaging
-  ];
-
-
-  propagatedBuildInputs = with python3Packages; [
-    setuptools
-    scikit-learn
-    matplotlib
-    sparse
-    pandas
-    dill
-    patsy
-    packaging
-  ];
-
-  nativeCheckInputs = with python3Packages; [
-    hypothesis
-  ];
 }
