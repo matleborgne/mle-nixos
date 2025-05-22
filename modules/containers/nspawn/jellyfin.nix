7a9103c959218917a12d1bc6d7ad86715c1f1007
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-22 22:15:21 +0000

    Create jellyfin.nix

---
+++ modules/containers/nspawn/jellyfin.nix
@@ -0,0 +1,118 @@
+{ lib, config, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CONTAINERS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Containers modules are the place for declaration of
+# nixos-containers, through nspawn from systemd
+
+{
+
+  options.mle.containers.nspawn.plexserver.enable = lib.mkOption {
+    description = "Configure plexserver nspawn container";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.containers.nspawn.plexserver.enable (
+
+    let
+      name = "plexserver";
+      net = (import ../../../secrets/keys/netIface);
+      address = (import ../../../secrets/containers_ips).plexserver;
+
+    in {
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Host prerequisites
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      mle.misc.nixos-containers.enable = lib.mkForce true;
+
+      systemd.tmpfiles.rules = [
+        "d /var/lib/plex - - - -"
+        "d /srv - - - -"
+      ];
+
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Container structure
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      containers.${name} = {
+
+        autoStart = true;
+        ephemeral = false;
+        privateNetwork = true;
+        macvlans = net.ifaceList;
+
+        bindMounts = {
+          "/var/lib/plex" = { hostPath = "/var/lib/plex"; isReadOnly = false; };
+          "/mnt/nfs/vid" = { hostPath = "/srv/vid"; isReadOnly = true; };
+          "/mnt/nfs/bkp/lxc/552-plexserver" = { hostPath = "/srv/bkp/lxc/552-plexserver"; isReadOnly = false; };
+          "/passfile" = { hostPath = "/etc/nixos/build/secrets/keys/restic_passfile"; isReadOnly = true; };
+        };
+
+        config = { lib, config, pkgs, options, ... }: {
+          system.stateVersion = "24.11";
+
+          networking.hostName = name;
+          systemd.network.networks."40-mv-${net.iface}" = { inherit address; };
+
+      
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Running services inside the container
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          systemd.tmpfiles.rules = [
+            "d /var/lib/plex 700 plex plex -"
+            "d /mnt/nfs/vid - - - -"
+            "d /mnt/nfs/bkp/lxc/552-plexserver - - - -"
+          ];
+
+          imports = [
+            ../../apps/plexserver.nix
+            ../../apps/fish.nix
+            ../../misc/networkd.nix
+          ];
+
+          mle = {
+            apps = {
+              plexserver.enable = true;
+              fish.enable = true;
+            };
+            misc = {
+              networkd.enable = true;
+            };
+          };
+
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # Backup service
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+          environment.systemPackages = with pkgs; [ restic ];
+
+          services.restic.backups = {
+
+            plex = {
+              initialize = false;
+              repository = "/mnt/nfs/bkp/lxc/552-plexserver";
+              paths = [ "/var/lib/plex" ];
+              passwordFile = "/passfile";
+              pruneOpts = [ "--keep-weekly 5" "--keep-monthly 3" ];
+              timerConfig = {
+                OnCalendar = "Tue 05:25";
+                Persistent = "true";
+              };
+            };
+
+          };
+
+
+
+        };
+      };
+  });
+}
