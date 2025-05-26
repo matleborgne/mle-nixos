d5d396cca2b0884b8488ae19c0b3982cd91ca8ad
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-26 19:04:11 +0000

    Update lx600Iso.nix

--- roles/lx600Iso.nix
+++ roles/lx600Iso.nix
@@ -22,13 +22,8 @@
   # Testing kernel because of new hardware
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
+  # Avoid double bootloader error
   boot.loader.grub.enable = false;
-
-  # Imports modules for iso creation
-  #imports = [
-  #  <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-  #  <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-  #];
 
 
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
