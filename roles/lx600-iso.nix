5413bc65f96a99b31d58e4081fee5f1bd57019f7
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-05-26 18:30:02 +0000

    Create lx600-iso.nix

---
+++ roles/lx600-iso.nix
@@ -0,0 +1,59 @@
+{ config, lib, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ROLES
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 1- Change base.nix config defined by "lib.mkDefault",
+# 2- Activate modules by mle.module.enable = true,
+
+# 3- Note it is possible to override config present in some module
+# To do that, use "lib.mkForce"
+
+{
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # 1- Modification of base.nix (defined by lib.mkDefault)
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  # Hostname
+  networking.hostName = "nix-lx600-iso";
+
+  # Testing kernel because of new hardware
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+
+
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # 2- Activation of mle.modules
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  mle = {
+    apps.librewolf.enable = true;
+    desktop.gnome.mleborgne.enable = true;
+
+    secrets = {
+      hm-mleborgne.enable = true;
+      openssh-server.enable = true;
+      openssh-client-mleborgne.enable = true;
+    };
+    
+  };
+
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # 3- Modification of mle.module (through lib.mkForce)
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  # Example with force bluetooth disabled
+  # Bad example because here we disable a full module, so we could do
+  # mle.bluetooth.enable = lib.mkForce false;
+  hardware.bluetooth.enable = lib.mkForce false;
+
+  # Works better for example with a gsettings parameter to override (font size, etc.)
+
+  environment.systemPackages = with pkgs; [
+    freefilesync
+  ];
+
+}
