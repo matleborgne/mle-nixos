65c7996f46234f5cf424884545c2a6b5df352b0e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-06-29 10:10:52 +0000

    Update mleborgne.nix

--- modules/desktop/hyprland/mleborgne.nix
+++ modules/desktop/hyprland/mleborgne.nix
@@ -31,23 +31,45 @@
 
     home-manager.users.mleborgne = {
 
-    
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Direct configuration
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      wayland.windowManager.hyprland.settings = {
+        "$mod" = "SUPER";
+
+        bind =
+          [
+            "$mod, F, exec, firefox"
+            ", Print, exec, grimblast copy area"
+          ]
+  
+          ++ (
+            # workspaces
+            # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+            builtins.concatLists (builtins.genList (i:
+                let ws = i + 1;
+                in [
+                  "$mod, code:1${toString i}, workspace, ${toString ws}"
+                  "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+                ]
+              )
+              9)
+         );
+
+        };
 
     };
       
+
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Plugins - prefered use with hyprland flake
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # Session GNOME
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            
-
-
-
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # Applications génériques
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            
-
+      wayland.windowManager.hyprland.plugins = [
+        inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
+        #"/absolute/path/to/plugin.so"
+      ];
 
 
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
