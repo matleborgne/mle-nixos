6bde936ac2d8ab5b49fc77ca0501fda71b916bce
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-06-29 10:18:05 +0000

    Update default.nix

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -31,14 +31,19 @@
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
+
+    # Avoid manual compilation
+    nix.settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
       
     services = {
-
       displayManager = {
         sddm.enable = true;
         sddm.wayland.enable = true;
-      };
-      
+      };     
     };
 
 
