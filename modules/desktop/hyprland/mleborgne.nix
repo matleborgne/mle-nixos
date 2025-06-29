9cf1bf70f3524da1b4abd8b8a47e598ca632024a
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-06-29 12:17:39 +0000

    Update mleborgne.nix

--- modules/desktop/hyprland/mleborgne.nix
+++ modules/desktop/hyprland/mleborgne.nix
@@ -107,9 +107,12 @@
         ### WORKSPACES - ALT BASED ###
         ###############################
 
-        #----- Mode view to workspace X -----
+        #----- Mode view through workspaces -----
+        bind = ALT, mouse_down, workspace, +1
+        bind = ALT, mouse_up, workspace, -1
         bind = ALT, right, workspace, +1
         bind = ALT, left, workspace, -1
+
         bind = ALT, code:10, workspace, 1
         bind = ALT, code:11, workspace, 2
         bind = ALT, code:12, workspace, 3
@@ -121,9 +124,12 @@
         bind = ALT, code:18, workspace, 9
         bind = ALT, code:19, workspace, 10
 
-        #----- Mode active window to workspace X -----
+        #----- Mode active window through workspaces -----
+        bind = ALT SHIFT, mouse_down, movetoworkspace, +1
+        bind = ALT SHIFT, mouse_up, movetoworkspace, -1
         bind = ALT SHIFT, right, movetoworkspace, +1
         bind = ALT SHIFT, left, movetoworkspace, -1
+
         bind = ALT SHIFT, code:10, movetoworkspace, 1
         bind = ALT SHIFT, code:11, movetoworkspace, 2
         bind = ALT SHIFT, code:12, movetoworkspace, 3
