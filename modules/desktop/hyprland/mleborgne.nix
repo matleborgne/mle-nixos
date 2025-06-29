e1355f83bf79b595d75bdd94d2e9263502e28c8d
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-06-29 11:01:17 +0000

    Update mleborgne.nix

--- modules/desktop/hyprland/mleborgne.nix
+++ modules/desktop/hyprland/mleborgne.nix
@@ -38,7 +38,6 @@
 
       settings = {
         "$mod" = "SUPER";
-        "$input.kb_layout" = "fr";
       };
 
 
@@ -47,6 +46,20 @@
       # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
       extraConfig = ''
+
+        #----- Input -----
+        input {
+          kb_layout = fr
+          kb_variant =
+          kb_model =
+          kb_options =
+          kb_rules =
+          follow_mouse = 1
+          touchpad {
+              natural_scroll = true
+          }
+          sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+        }     
 
         #----- Windows resize -----
         bind = $mod, S, submap, resize
