8555f25c7a27023bddebdbd77329544d3e83fb23
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-07-19 12:06:03 +0000

    Create default.nix

---
+++ modules/desktop/i3/mleborgne/default.nix
@@ -0,0 +1,52 @@
+{ inputs, config, lib, pkgs, ... }:
+
+{
+
+  options.mle.desktop.i3.mleborgne.enable = lib.mkOption {
+    description = "mleborgne customized i3";
+    type = lib.types.bool;
+    default = false;
+  };
+  
+  config = lib.mkIf config.mle.desktop.i3.mleborgne.enable {
+  
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    mle = {
+      
+      desktop.i3 = {
+        default.enable = true;
+      };
+      hardware.printing.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      libinput libinput-gestures wmctrl
+    ];
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Hyprland configuration
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Other basics configurations
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    home-manager.users.mleborgne.home.file = {
+
+      ".local/share/rofi/themes/rofi.rasi" = {
+        enable = true;
+        text = builtins.readFile ./rofi.rasi;
+      };
+
+    };
+
+
+  };
+}
