b8eeeebab002163a293830e16c904d995bc8a9c7
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-07-19 12:04:13 +0000

    Update default.nix

--- modules/desktop/i3/default.nix
+++ modules/desktop/i3/default.nix
@@ -1,1 +1,85 @@
+{ inputs, config, lib, pkgs, ... }:
 
+{
+
+  options.mle.desktop.hyprland.default.enable = lib.mkOption {
+    description = "Enable i3";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.desktop.i3.default.enable {
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    mle.apps.logitech.enable = true;
+
+    mle.desktop.codecs.enable = true;
+    mle.desktop.pipewire.enable = true;
+
+    mle.misc.networkmanager.enable = true;
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Base of i3 desktop
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    services.xserver.windowManager.i3 = {
+      enable = true;
+    };
+
+    services.desktopManager = {
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+
+    services.displayManager = {
+      lightdm.enable = true;
+      defaultSession = "xfce+i3";
+    };
+      
+    services = {
+      libinput.enable = true;    
+    };
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Added packages
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    environment.systemPackages = with pkgs; [
+      kitty
+      pavucontrol
+      nautilus
+
+      adw-gtk3
+      papirus-icon-theme
+      ubuntu_font_family
+    ];
+
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Fixing problems with themes (system-wide solution)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      
+    programs.dconf.profiles.user.databases = [
+      {
+        settings."org/gnome/desktop/interface" = {
+          gtk-theme = "Adwaita";
+          icon-theme = "Flat-Remix-Red-Dark";
+          font-name = "Noto Sans Medium 11";
+          document-font-name = "Noto Sans Medium 11";
+          monospace-font-name = "Noto Sans Mono Medium 11";
+        };
+      }
+    ];
+      
+    
+  };
+}
