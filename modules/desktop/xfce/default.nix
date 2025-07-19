eb81efb54136e10425d4690424813a39b9cce667
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-07-19 20:51:22 +0000

    Update default.nix

--- modules/desktop/xfce/default.nix
+++ modules/desktop/xfce/default.nix
@@ -1,1 +1,84 @@
+{ inputs, config, lib, pkgs, ... }:
 
+{
+
+  options.mle.desktop.xfce.default.enable = lib.mkOption {
+    description = "Enable xfce";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.desktop.xfce.default.enable {
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    mle.apps.logitech.enable = true;
+
+    mle.desktop.codecs.enable = true;
+    mle.desktop.pipewire.enable = true;
+
+    mle.misc.networkmanager.enable = true;
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Base of i3 desktop
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+  };
+  services.displayManager.defaultSession = "xfce";
+
+    services.xserver.displayManager = {
+      lightdm.enable = true;
+      defaultSession = "xfce";
+    };
+      
+    services = {
+      libinput.enable = true;    
+    };
+
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Added packages
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    environment.systemPackages = with pkgs; [
+      kitty
+      pavucontrol
+      nautilus
+
+      adw-gtk3
+      papirus-icon-theme
+      ubuntu_font_family
+    ];
+
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Fixing problems with themes (system-wide solution)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      
+    programs.dconf.profiles.user.databases = [
+      {
+        settings."org/gnome/desktop/interface" = {
+          gtk-theme = "Adwaita";
+          icon-theme = "Flat-Remix-Red-Dark";
+          font-name = "Noto Sans Medium 11";
+          document-font-name = "Noto Sans Medium 11";
+          monospace-font-name = "Noto Sans Mono Medium 11";
+        };
+      }
+    ];
+      
+    
+  };
+}
