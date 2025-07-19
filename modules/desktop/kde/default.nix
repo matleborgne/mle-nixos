909a5bf566567f2850eff4ad87a3af9a997a44fa
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-07-19 22:34:11 +0000

    Update default.nix

--- modules/desktop/kde/default.nix
+++ modules/desktop/kde/default.nix
@@ -1,1 +1,76 @@
+{ config, lib, pkgs, ... }:
 
+{
+
+  options.mle.desktop.kde.default.enable = lib.mkOption {
+    description = "Enable KDE plasma";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.desktop.kde.default.enable {
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    mle.apps.logitech.enable = true;
+
+    mle.desktop.codecs.enable = true;
+    mle.desktop.pipewire.enable = true;
+
+    mle.misc.networkmanager.enable = true;
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Base of KDE PLASMA desktop
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    services = {
+      udev.packages = [ pkgs.gnome-settings-daemon ];
+
+      xserver = {
+        excludePackages = [ pkgs.xterm ];
+        xkb = {
+          layout = "fr";
+          variant = "azerty";
+          options = "eurosign:e";
+        };
+      };
+
+      displayManager.sddm.enable = true;
+      desktopManager.plasma6 = {
+        enable = true;
+      };
+    };
+
+    # GNOME desktop integration
+    qt = {
+      enable = true;
+      platformTheme = "gnome";
+      style = "adwaita-dark";
+    };
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Packages exclusion
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    environment.plasma6.excludePackages = with pkgs; [
+      oxygen kdepim-runtime
+    ];
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Added packages
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    programs.kdeconnect = {
+      enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+   
+    ];
+
+  };
+}
