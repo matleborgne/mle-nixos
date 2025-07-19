77e24c6032c0ff155269a12ad1acf47df0db689a
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-07-19 15:22:39 +0000

    Update gamestation.nix

--- modules/desktop/gnome/gamestation.nix
+++ modules/desktop/gnome/gamestation.nix
@@ -1,1 +1,39 @@
+{ config, lib, pkgs, ... }:
 
+{
+
+  options.mle.desktop.gnome.autologin.enable = lib.mkOption {
+    description = "GNOME autologin";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.desktop.gnome.autologin.enable (
+
+
+  let
+    allUsers = builtins.attrNames config.users.users;
+    normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
+    user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
+
+  in {
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Module
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    services.xserver.displayManager.gdm.autoLogin = {
+      enable = true;
+      inherit user;
+    };
+
+    services.xserver.displayManager.gdm.wayland = false;
+    services.displayManager.defaultSession = "gnome-xorg";
+
+  });
+}
