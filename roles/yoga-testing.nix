501a9fd59f3e02c09df4f80a28fdfe344670a35d
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-07-19 20:51:51 +0000

    Update yoga-testing.nix

--- roles/yoga-testing.nix
+++ roles/yoga-testing.nix
@@ -45,7 +45,7 @@
     };
 
     #desktop.gnome.mleborgne.enable = true;
-    desktop.i3.mleborgne.enable = true;
+    desktop.xfce.default.enable = true;
 
     flatpaks = {
       vscode.enable = true;
