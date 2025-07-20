a05a1a7da113ac03218dd13b6cdca0546086b724
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-07-20 11:01:07 +0000

    Create qdbus.sh

---
+++ modules/desktop/kde/gamestation/qdbus.sh
@@ -0,0 +1,9 @@
+#!/run/current-system/sw/bin/bash
+
+# Autohide PANEL
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
+                      var panel = panels()[0];
+                      panel.hiding = \"autohide\";
+                      "
+
+#
