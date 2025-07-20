5c8a1a8ef4269378935ac2b586ff22940a2274e5
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-07-20 13:41:28 +0000

    Update qdbus.sh

--- modules/desktop/kde/gamestation/qdbus.sh
+++ modules/desktop/kde/gamestation/qdbus.sh
@@ -5,7 +5,3 @@ qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
                       var panel = panels()[0];
                       panel.hiding = \"autohide\";
                       "
-
-# Mode performance
-# Authorize sudo access for this command
-sudo powerprofilesctl set performance
