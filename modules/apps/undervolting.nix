b0c39e5ecbfebb9ed2cae68bd3afcafb95f3d2b1
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-08-25 16:51:36 +0000

    Update undervolting.nix

--- modules/apps/undervolting.nix
+++ modules/apps/undervolting.nix
@@ -1,1 +1,40 @@
+{ lib, config, pkgs, ... }:
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# APPS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Apps modules are the place for customization of individual apps
+# This is not the place for bundles, hardware, desktop, etc.
+# Only for individual applications
+
+{
+
+  options.mle.apps.undervolting.enable = lib.mkOption {
+    description = "Configure UNDERVOLTING app";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.mle.apps.undervolting.enable {
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Activation and customization of APP
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    environment.systemPackages = with pkgs; [
+      lact
+    ];
+
+  systemd
+
+
+    
+  };
+}
