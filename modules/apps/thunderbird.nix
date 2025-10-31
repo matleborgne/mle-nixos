1be34e8ed56bab6cb17704287a12a4f0a8815d89
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-10-31 08:54:52 +0000

    Update thunderbird.nix

--- modules/apps/thunderbird.nix
+++ modules/apps/thunderbird.nix
@@ -18,13 +18,6 @@
 
   config = lib.mkIf config.mle.apps.thunderbird.enable {
 
-  let
-    allUsers = builtins.attrNames config.users.users;
-    normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
-    user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
-
-  in {
-
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Recursive activation of other mle.<modules>
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
