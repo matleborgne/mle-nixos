67f26aa76ae4ee2f6b735ab2a2f9772ee54aa8aa
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-11-29 23:34:01 +0000

    Update openrgb.nix

--- modules/apps/openrgb.nix
+++ modules/apps/openrgb.nix
@@ -30,7 +30,6 @@
 
     services.udev.packages = [ pkgs.openrgb ];
     boot.kernelModules = [ "i2c-dev" ];
-    hardware.i2c.enable = true;
 
     services.hardware.openrgb = { 
       enable = true; 
