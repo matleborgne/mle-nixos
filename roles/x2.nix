4a6b231caef49b48b70772e19a53e81a21fd514c
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-12-21 16:19:51 +0000

    Update x2.nix

--- roles/x2.nix
+++ roles/x2.nix
@@ -89,7 +89,7 @@
 
   # Works better for example with a gsettings parameter to override (font size, etc.)
 
-  security.pki.certificateFiles = [];
+  security.pki.certificateFiles = import (builtins.toPath ./secrets/keys/cert/import.nix);
 
 
 }
