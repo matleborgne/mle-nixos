23ec8c084335b833a6ce964f1b984747ee32176e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-06 12:34:48 +0000

    Create kdenlive.nix

---
+++ modules/flatpaks/kdenlive.nix
@@ -0,0 +1,56 @@
+{ config, lib, pkgs, ... }:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# FLATPAKS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Flatpaks are flatpak apps declarative configuration
+# Pay attention that flatpak has risk to be inpure
+# As it could not be avoided users imperative commands
+# directly in the terminal
+# So avoid using flatpaks in strict pure evaluations
+
+{
+
+  options.mle.flatpaks.kdenlive.enable = lib.mkOption {
+    description = "Enable KDENLIVE flatpak";
+    type = lib.types.bool;
+    default = false;
+  };
+  
+  config = lib.mkIf config.mle.flatpaks.kdenlive.enable (
+
+  let
+    allUsers = builtins.attrNames config.users.users;
+    normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
+    user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
+
+  in {
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Recursive activation of other mle.<modules>
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    mle.misc.flatpak.enable = lib.mkForce true;
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Flatpak configuration
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    systemd.user.services.flatpak-kdenlive = {
+      wantedBy = [ "default.target" ];
+      path = [ pkgs.bash pkgs.flatpak ];
+      script = ''
+        flatpak install --or-update --noninteractive org.kde.kdenlive
+
+        cat << EOF | flatpak run --command=/bin/bash org.kde.kdenlive
+          # TODO : command line for installing SAM2 object detection
+          # and other AI tools
+        EOF
+      '';
+    };
+    
+  });
+}
