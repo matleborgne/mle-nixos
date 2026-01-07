f31d83c0d209735cf633d7010bb1e99f2ebab840
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:22:07 +0000

    Create extensions-vitals.sh

---
+++ modules/desktop/gnome/gsettings/extensions-vitals.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ~~~~~~~~~~ Vitals ~~~~~~~~~~
+gsettings set org.gnome.shell.extensions.vitals update-time 3
+gsettings set org.gnome.shell.extensions.vitals hide-icons true
+gsettings set org.gnome.shell.extensions.vitals fixed-widths true
+gsettings set org.gnome.shell.extensions.vitals show-system false
+gsettings set org.gnome.shell.extensions.vitals show-fan false
+gsettings set org.gnome.shell.extensions.vitals show-voltage false
+gsettings set org.gnome.shell.extensions.vitals show-network false
+gsettings set org.gnome.shell.extensions.vitals show-storage false
+gsettings set org.gnome.shell.extensions.vitals show-battery true
+gsettings set org.gnome.shell.extensions.vitals battery-slot 1
