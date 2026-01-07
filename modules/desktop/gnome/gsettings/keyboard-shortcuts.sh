4bbbcc1d7a24050ecb8e8d0abfedb2ebb706271e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 11:59:18 +0000

    Create keyboard-shortcuts.sh

---
+++ modules/desktop/gnome/gsettings/keyboard-shortcuts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ~~~~~~~~~~ Raccourcis clavier ~~~~~~~~~~
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Alt><Shift>Left']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Alt><Shift>Right']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Alt>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Alt>Right']"
