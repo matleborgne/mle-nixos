cbf459ef5a860d78257a710ae08fc14d9cbfd4d9
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:20:09 +0000

    Create extensions-pop-shell.sh

---
+++ modules/desktop/gnome/gsettings/extensions-pop-shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ~~~~~~~~~~ Pop shell ~~~~~~~~~~
+gsettings set org.gnome.shell.extensions.pop-shell tile-by-default true
+gsettings set org.gnome.shell.extensions.pop-shell gap-inner 4
+gsettings set org.gnome.shell.extensions.pop-shell gap-outer 4
