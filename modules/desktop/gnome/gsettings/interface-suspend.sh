c8e825346f56d97e1af0768c183d36eb8daaa66f
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 13:05:51 +0000

    Update interface-suspend.sh

--- modules/desktop/gnome/gsettings/interface-suspend.sh
+++ modules/desktop/gnome/gsettings/interface-suspend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # ~~~~~~~~~~ Désactivation de la mise en veille ~~~~~~~~~~
+export GSETTINGS_SCHEMA_DIR=$(grep -r settings-daemon ./.gschemas | tail -n 1)
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type "nothing"
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"
 
