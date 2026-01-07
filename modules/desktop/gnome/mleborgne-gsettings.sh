c84ad20f7ad89ea4b8cfbd5b7aef46d357641df5
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 11:27:43 +0000

    Update mleborgne-gsettings.sh

--- modules/desktop/gnome/mleborgne-gsettings.sh
+++ modules/desktop/gnome/mleborgne-gsettings.sh
@@ -11,8 +11,19 @@ gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 3.
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 4
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
 
+# Police et taille d'interface
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.3
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface font-name "Ubuntu Regular 11"
+gsettings set org.gnome.desktop.interface document-font-name "Ubuntu Regular 11"
+gsettings set org.gnome.desktop.interface monospace-font-name "Ubuntu Mono Regular 12"
+gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus"
+
 # Raccourcis clavier
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Alt><Shift>Left']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Alt><Shift>Right']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Alt>Left']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Alt>Right']"
+
+
