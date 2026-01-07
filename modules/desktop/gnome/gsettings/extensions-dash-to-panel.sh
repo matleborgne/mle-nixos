d9bc0c7c6f594c159b4d897d9667cdbf09083e83
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:27:25 +0000

    Create extensions-dash-to-panel.sh

---
+++ modules/desktop/gnome/gsettings/extensions-dash-to-panel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ~~~~~~~~~~ Dash to panel ~~~~~~~~~~
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-style-focused "DASHES"
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-style-unfocused "DOTS"
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-position "TOP"
+gsettings set org.gnome.shell.extensions.dash-to-panel focus-highlight-opacity 15
+gsettings set org.gnome.shell.extensions.dash-to-panel panel-sizes "{'0':40,'1':40}"
+gsettings set org.gnome.shell.extensions.dash-to-panel panel-positions "{'0':'TOP','1':'TOP'}"
+gsettings set org.gnome.shell.extensions.dash-to-panel stockgs-keep-dash true
+gsettings set org.gnome.shell.extensions.dash-to-panel trans-panel-opacity 0.0
+gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-custom-opacity true
+gsettings set org.gnome.shell.extensions.dash-to-panel animate-appicon-hover false
+gsettings set org.gnome.shell.extensions.dash-to-panel appicon-padding 4
+gsettings set org.gnome.shell.extensions.dash-to-panel appicon-margin 1
+gsettings set org.gnome.shell.extensions.dash-to-panel show-favorites false
+gsettings set org.gnome.shell.extensions.dash-to-panel show-activities-button false
+gsettings set org.gnome.shell.extensions.dash-to-panel tray-size 20
+gsettings set org.gnome.shell.extensions.dash-to-panel status-icon-padding 4
+gsettings set org.gnome.shell.extensions.dash-to-panel panel-element-positions "{
+
+{"DEL-5R5RW83":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":true,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"dateMenu","visible":true,"position":"centerMonitor"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":false,"position":"stackedBR"}],"GSM-303MAHUFJ457":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":true,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"dateMenu","visible":true,"position":"centerMonitor"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":false,"position":"stackedBR"}]}
