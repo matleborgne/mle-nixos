bf2f4e84cfc65918a3fb96930870b8c0207118fa
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:23:02 +0000

    Create extensions-appindicator.sh

---
+++ modules/desktop/gnome/gsettings/extensions-appindicator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# ~~~~~~~~~~ Appindicator ~~~~~~~~~~
+gsettings set org.gnome.shell.extensions.appindicator tray-pos "center"
+gsettings set org.gnome.shell.extensions.appindicator icon-brightness -0.3
