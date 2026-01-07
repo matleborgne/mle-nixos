327370d22fc1c03839917b3dee6af317f54896c7
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 13:00:15 +0000

    Update extensions-user-theme.sh

--- modules/desktop/gnome/gsettings/extensions-user-theme.sh
+++ modules/desktop/gnome/gsettings/extensions-user-theme.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# ~~~~~~~~~~ Schemas DIR ~~~~~~~~~~
+export GSETTINGS_SCHEMA_DIR=$(grep -r user-theme ./.gschemas | tail -n 1)
+
 # ~~~~~~~~~~ User theme ~~~~~~~~~~
 gsettings set org.gnome.shell.extensions.user-theme name "Fluent-round-Dark"
