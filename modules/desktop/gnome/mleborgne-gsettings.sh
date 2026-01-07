4c51817b6fcdc29b2c5e8178d99cc9677e488fb1
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 11:22:41 +0000

    Create mleborgne-gsettings.sh

---
+++ modules/desktop/gnome/mleborgne-gsettings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Paramètres généraux de GNOME
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Mode nuit - éclairage lumière bleue
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 4700
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 3.9999
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 4
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
