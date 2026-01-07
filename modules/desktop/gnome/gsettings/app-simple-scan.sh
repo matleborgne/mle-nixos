84cd4c191b6d2f869422ba81b99efa37e65e93ae
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 13:07:11 +0000

    Update app-simple-scan.sh

--- modules/desktop/gnome/gsettings/app-simple-scan.sh
+++ modules/desktop/gnome/gsettings/app-simple-scan.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ~~~~~~~~~~ Schemas DIR ~~~~~~~~~~
+export GSETTINGS_SCHEMA_DIR=$(grep -r simple-scan ./.gschemas | tail -n 1)
+
 # ~~~~~~~~~~ Scanner ~~~~~~~~~~
 gsettings set org.gnome.simple-scan jpeg-quality 23
 gsettings set org.gnome.SimpleScan jpeg-quality 23
