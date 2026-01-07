986d0333506e8d1472fc977f3961fd6938b742a3
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 11:58:32 +0000

    Update interface-fonts.sh

--- modules/desktop/gnome/gsettings/interface-fonts.sh
+++ modules/desktop/gnome/gsettings/interface-fonts.sh
@@ -13,3 +13,16 @@ gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,c
 gsettings set org.gnome.desktop.wm.preferences theme "adw-gtk3"
 gsettings set org.gnome.desktop.wm.preferences titlebar-font "Ubuntu Bold 11"
 gsettings set org.gnome.desktop.wm.preferences focus-mode "click"
+
+# ~~~~~~~~~~ Session mutter ~~~~~~~~~~
+gsettings set org.gnome.mutter check-alive-timeout 30000
+gsettings set org.gnome.mutter dynamic-workspaces true
+gsettings set org.gnome.mutter edge-tiling true
+gsettings set org.gnome.mutter workspaces-only-on-primary true
+
+gsettings set org.gnome.system.location enabled false
+
+gsettings set org.gnome.desktop.background color-shading-type "solid"
+gsettings set org.gnome.desktop.background picture-options "zoom"
+gsettings set org.gnome.desktop.background picture-uri "file:////$HOME/.local/share/.wallpaper.jpg";
+gsettings set org.gnome.desktop.background picture-uri-dark "file:////$HOME/.local/share/.wallpaper.jpg";
