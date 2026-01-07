f508c9075ee2764c55963fdd323c7f8e1d9624da
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:59:43 +0000

    Update extensions-pop-shell.sh

--- modules/desktop/gnome/gsettings/extensions-pop-shell.sh
+++ modules/desktop/gnome/gsettings/extensions-pop-shell.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ~~~~~~~~~~ Schemas DIR ~~~~~~~~~~
+export GSETTINGS_SCHEMA_DIR=$(grep -r pop-shell ./.gschemas | tail -n 1)
+
 # ~~~~~~~~~~ Pop shell ~~~~~~~~~~
 gsettings set org.gnome.shell.extensions.pop-shell tile-by-default true
 gsettings set org.gnome.shell.extensions.pop-shell gap-inner 4
