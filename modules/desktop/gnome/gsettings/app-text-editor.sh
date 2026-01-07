2481db4bdbc2ccea6fa5d52bfe36a800c6153ba0
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 13:07:35 +0000

    Update app-text-editor.sh

--- modules/desktop/gnome/gsettings/app-text-editor.sh
+++ modules/desktop/gnome/gsettings/app-text-editor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ~~~~~~~~~~ Schemas DIR ~~~~~~~~~~
+export GSETTINGS_SCHEMA_DIR=$(grep -r text-editor ./.gschemas | tail -n 1)
+
 # ~~~~~~~~~~ Text Editor ~~~~~~~~~~
 gsettings set org.gnome.TextEditor restore-session false
 gsettings set org.gnome.TextEditor show-line-numbers true
