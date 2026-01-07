9888a0bcf58ebee697016bdb42a5144fd6f65290
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 13:03:38 +0000

    Update interface-night-mode.sh

--- modules/desktop/gnome/gsettings/interface-night-mode.sh
+++ modules/desktop/gnome/gsettings/interface-night-mode.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ~~~~~~~~~~ Schemas DIR ~~~~~~~~~~
+export GSETTINGS_SCHEMA_DIR=$(grep -r settings-daemon ./.gschemas | tail -n 1)
+
 # ~~~~~~~~~~ Mode nuit - éclairage lumière bleue ~~~~~~~~~~
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 4700
