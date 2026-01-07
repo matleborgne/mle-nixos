beae8b2951d32b48c81ab0fbd655ffe9ba28906d
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:49:59 +0000

    Update _schemadir.sh

--- modules/desktop/gnome/gsettings/_schemadir.sh
+++ modules/desktop/gnome/gsettings/_schemadir.sh
@@ -8,5 +8,5 @@ if [ $distro == "nixos" ] ; then
   echo $(find -L "$HOME/.local/share/gnome-shell/" -type d -name "schemas") >> ./.gschemas
   sed -i "s/ /\n/g" .gschemas
 else
-  schemadir="/usr/share/glib-2.0/schemas"
+  echo "/usr/share/glib-2.0/schemas" > ./.gschemas
 elif
