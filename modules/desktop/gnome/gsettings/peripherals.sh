d4b8016aea1b7807b3f4da7e6a48c2ed034fd67e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 11:59:53 +0000

    Update and rename keyboard-shortcuts.sh to peripherals.sh

--- modules/desktop/gnome/gsettings/peripherals.sh
+++ modules/desktop/gnome/gsettings/peripherals.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# ~~~~~~~~~~ Souris et touchpad ~~~~~~~~~~
+gsettings set org.gnome.desktop.peripherals.touchpad click-method "areas"
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
+gsettings set org.gnome.desktop.peripherals.touchpad speed .25
+
+gsettings set org.gnome.desktop.peripherals.mouse speed .25
+
 # ~~~~~~~~~~ Raccourcis clavier ~~~~~~~~~~
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Alt><Shift>Left']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Alt><Shift>Right']"
