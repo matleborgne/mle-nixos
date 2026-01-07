949f0e7356fa320086b56489024da28f63ddfe79
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:59:12 +0000

    Update extensions-just-perfection.sh

--- modules/desktop/gnome/gsettings/extensions-just-perfection.sh
+++ modules/desktop/gnome/gsettings/extensions-just-perfection.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ~~~~~~~~~~ Schemas DIR ~~~~~~~~~~
+export GSETTINGS_SCHEMA_DIR=$(grep -r just-perfection ./.gschemas | tail -n 1)
+
 # ~~~~~~~~~~ Just perfection ~~~~~~~~~~
 gsettings set org.gnome.shell.extensions.just-perfection dash-separator false
 gsettings set org.gnome.shell.extensions.just-perfection search false
