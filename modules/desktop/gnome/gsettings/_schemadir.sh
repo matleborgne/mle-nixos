dbc1416d260e461d4f447df96271cc19926078b9
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:42:34 +0000

    Create _schemadir.sh

---
+++ modules/desktop/gnome/gsettings/_schemadir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+distro = $(cat /etc/os-release | grep '^ID=' | awk -F '=' '{print $NF }')
+
+if [ $distro == "nixos" ] ; then
+  echo $(find -L "/run/current-system/sw/share/gsettings-schemas/" -type d -name "schemas") > ./.gschemas
+  echo $(find -L "/run/current-system/sw/share/gnome-shell/extensions/" -type d -name "schemas") >> ./.gschemas
+  echo $(find -L "$HOME/.local/share/gnome-shell/" -type d -name "schemas") >> ./.gschemas
+  sed -i "s/ /\n/g" .gschemas
+else
+  pass
+elif
