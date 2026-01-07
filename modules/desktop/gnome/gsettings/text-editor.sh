e8bac4f6b79dfdb7f5c0ce9a6b5e8e1924c776e1
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:08:06 +0000

    Create text-editor.sh

---
+++ modules/desktop/gnome/gsettings/text-editor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ~~~~~~~~~~ Text Editor ~~~~~~~~~~
+gsettings set org.gnome.TextEditor restore-session false
+gsettings set org.gnome.TextEditor show-line-numbers true
+gsettings set org.gnome.TextEditor discover-settings false
