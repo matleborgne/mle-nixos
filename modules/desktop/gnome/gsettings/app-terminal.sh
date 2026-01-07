d24eb2686261ae5d2e357089610cd00f13d559bd
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 13:02:37 +0000

    Update app-terminal.sh

--- modules/desktop/gnome/gsettings/app-terminal.sh
+++ modules/desktop/gnome/gsettings/app-terminal.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export GSETTINGS_SCHEMA_DIR=$(grep -r gnome-terminal ./.gschemas | tail -n 1)
 gsettings set org.gnome.terminal.legacy theme-variant dark
+
+export GSETTINGS_SCHEMA_DIR=$(grep -r ptyxis ./.gschemas | tail -n 1)
 gsettings set org.gnome.Ptyxis interface-style dark
