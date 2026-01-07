2b51d9fae16936afb25cbb23390a744f6fa67834
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 11:25:33 +0000

    Update mleborgne-gsettings.sh

--- modules/desktop/gnome/mleborgne-gsettings.sh
+++ modules/desktop/gnome/mleborgne-gsettings.sh
@@ -10,3 +10,9 @@ gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 47
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 3.9999
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 4
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
+
+# Raccourcis clavier
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Alt><Shift>Left']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Alt><Shift>Right']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Alt>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Alt>Right']"
