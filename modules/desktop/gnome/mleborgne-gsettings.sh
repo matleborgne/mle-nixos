a6ccbcd561c79d455d5275f53a0323a23f7cf23d
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 11:29:49 +0000

    Update mleborgne-gsettings.sh

--- modules/desktop/gnome/mleborgne-gsettings.sh
+++ modules/desktop/gnome/mleborgne-gsettings.sh
@@ -4,14 +4,15 @@
 # Paramètres généraux de GNOME
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Mode nuit - éclairage lumière bleue
+# ~~~~~~~~~~ Mode nuit - éclairage lumière bleue ~~~~~~~~~~
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 4700
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 3.9999
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 4
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
 
-# Police et taille d'interface
+
+# ~~~~~~~~~~ Police et taille d'interface ~~~~~~~~~~
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.3
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface font-name "Ubuntu Regular 11"
@@ -20,7 +21,13 @@ gsettings set org.gnome.desktop.interface monospace-font-name "Ubuntu Mono Regul
 gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3"
 gsettings set org.gnome.desktop.interface icon-theme "Papirus"
 
-# Raccourcis clavier
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,close"
+gsettings set org.gnome.desktop.wm.preferences theme "adw-gtk3"
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Ubuntu Bold 11"
+gsettings set org.gnome.desktop.wm.preferences focus-mode "click"
+
+
+# ~~~~~~~~~~ Raccourcis clavier ~~~~~~~~~~
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Alt><Shift>Left']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Alt><Shift>Right']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Alt>Left']"
