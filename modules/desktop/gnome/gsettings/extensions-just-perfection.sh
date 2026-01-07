9bea3459f2170710f22e642b9c01666c18dc3d6d
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-07 12:17:04 +0000

    Create extensions-just-perfection.sh

---
+++ modules/desktop/gnome/gsettings/extensions-just-perfection.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# ~~~~~~~~~~ Just perfection ~~~~~~~~~~
+gsettings set org.gnome.shell.extensions.just-perfection dash-separator false
+gsettings set org.gnome.shell.extensions.just-perfection search false
