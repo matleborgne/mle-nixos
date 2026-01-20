2cd4036789434708a9ec68d90e6aa592b54ad486
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-20 23:09:30 +0000

    Update amdgpu.nix

--- modules/hardware/amdgpu.nix
+++ modules/hardware/amdgpu.nix
@@ -24,13 +24,11 @@
     
     hardware.graphics = {
       enable = true;
+      enable32Bit = true;
 
-      #driSupport = lib.mkDefault true;
       extraPackages = with pkgs; [
         rocmPackages.clr.icd
       ];
-
-      #driSupport32Bit = lib.mkDefault true;
 
     };
 
