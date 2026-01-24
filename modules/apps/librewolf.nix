87eb7e6723ee6dda036edf0221b47d9abd0030ba
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-24 11:06:19 +0000

    Update librewolf.nix

--- modules/apps/librewolf.nix
+++ modules/apps/librewolf.nix
@@ -58,7 +58,6 @@
             (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
             (extension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
             (extension "istilldontcareaboutcookies" "idcac-pub@guus.ninja")
-            (extension "return-youtube-dislikes" "{762f9885-5a13-4abd-9c77-433dcd38b8fd}")
           ];
 
       };
