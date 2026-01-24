0b1192b95c37edad3be6be95af52e7b680cbbb36
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-24 15:22:07 +0000

    Update codecs.nix

--- modules/desktop/codecs.nix
+++ modules/desktop/codecs.nix
@@ -22,13 +22,23 @@
     
     environment.systemPackages = with pkgs; [
       gst_all_1.gstreamer
-      gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good
-      gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
+      gst_all_1.gst-libav
+      gst_all_1.gst-vaapi
 
       mesa driversi686Linux.mesa
-      ffmpeg
-    ];
+    ] ++
+
+    [(pkgs.ffmpeg-full.override {
+      withUnfree = true;
+    })];
+
+    environment.variables = {
+      GST_PLUGIN_PATH = "/run/current-system/sw/lib/gstreamer-1.0/";
+    };
 
   };
-
 }
