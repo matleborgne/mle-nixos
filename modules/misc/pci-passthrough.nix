5b2acf1e8aa9a3e4d701fe7fb0e01a5941005e8c
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-01-27 09:09:29 +0000

    Update pci-passthrough.nix

--- modules/misc/pci-passthrough.nix
+++ modules/misc/pci-passthrough.nix
@@ -45,7 +45,8 @@
     boot.kernelParams = [ 
       "amd_iommu=on"
       "iommu=pt"
-      "pcie_acs_override=multifunction,id:10de:2d05,id:10de:22eb"
+      #"pcie_acs_override=multifunction,id:10de:2d05,id:10de:22eb"
+      "pcie_acs_override=downstream,multifunction"
       "vfio-pci.ids=10de:2d05,10de:22eb" # replace with yours (lspci -nnk)
     ];
 
