35ccc427e33473ab20398793702196055d1febe0
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-08 00:41:07 +0200

    Update codium.nix

--- modules/apps/codium.nix
+++ modules/apps/codium.nix
@@ -30,7 +30,7 @@
       scikit-learn statsmodels
       jellyfish chardet levenshtein
       sparse dill patsy
-      flashtext chainladder
+      # flashtext chainladder
       # hdbscan            # cf. note plus bas
       # scikit-learn-extra # cf. note plus bas
     ]);
