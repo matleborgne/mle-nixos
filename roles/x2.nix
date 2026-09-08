4c1214306af94880de263708abcc6ed107450691
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-08 17:45:16 +0200

    Update x2.nix

--- roles/x2.nix
+++ roles/x2.nix
@@ -33,6 +33,7 @@
       ddcutil.enable = true;
       firefox.enable = true;
       jellyfin.enable = true;
+      kdenlive.enable = true;
       logitech.enable = true;
       thunderbird.enable = true;      
       video-downloader.enable = true; 
