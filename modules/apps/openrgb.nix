b11106b17ff975823e1d2c3ed43c0551c1fa0dae
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-08 16:56:53 +0200

    Update openrgb.nix

--- modules/apps/openrgb.nix
+++ modules/apps/openrgb.nix
@@ -16,7 +16,14 @@
     default = false;
   };
 
-  config = lib.mkIf config.mle.apps.openrgb.enable {
+  config = lib.mkIf config.mle.apps.openrgb.enable (
+
+  let
+    allUsers = builtins.attrNames config.users.users;
+    normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
+    user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
+
+  in {
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Recursive activation of other mle.<modules>
@@ -58,5 +65,5 @@
     };
 
 
-  };
+  });
 }
