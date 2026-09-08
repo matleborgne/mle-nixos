a9520d38727ca8739d4c4007b7afed96ec5e6cdb
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-08 17:44:38 +0200

    Update openrgb.nix

--- modules/apps/openrgb.nix
+++ modules/apps/openrgb.nix
@@ -56,11 +56,11 @@
     systemd.services.openrgb-gskill = {
       wantedBy = [ "multi-user.target" ];
       script = ''
-        [Desktop Entry]
+        echo "[Desktop Entry]
         Type=Application
         Name=GskillRGB
         Exec=openrgb --profile gskillOff
-        X-GNOME-Autostart-enabled=true > /home/"${user}"/.config/autostart/gskill.desktop
+        X-GNOME-Autostart-enabled=true" > /home/"${user}"/.config/autostart/gskill.desktop
       '';
     };
 
