4c648d78c017393979197b697b697c61176292c1
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-09 17:30:14 +0200

    Update nvidia.nix

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -46,6 +46,12 @@
       ];
     };
 
+    # Required for hibernation
+    boot.extraModprobeConfig = ''
+      options nvidia NVreg_PreserveVideoMemoryAllocations=1
+      options nvidia NVreg_TemporaryFilePath=/var/tmp
+    '';
+
     # A date, NixOS continue d'utiliser xserver pour nvidia
     services.xserver = {
       videoDrivers = [ "nvidia" ];
@@ -56,47 +62,17 @@
       __GL_SHADER_DISK_CACHE_SIZE = "12000000000";
     };
 
+    boot.blacklistedKernelModules = [
+      "nouveau"
+      "nova_core"
+    ];
 
     boot.kernelParams = [
       "module_blacklist=amdgpu,i915"
       "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
       "NVreg_TemporaryFilePath=/var/tmp"
     ];
-    
 
-    
-    hardware.nvidia = {
-      modesetting.enable = true;
-
-      # Driver version : stable, beta, production (older), vulkan_beta, legacy_... (check nixpkgs website)
-      package = config.boot.kernelPackages.nvidiaPackages.latest;
-
-      # Enable the Nvidia settings menu, accessible via `nvidia-settings`
-      nvidiaSettings = true;
-
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-      powerManagement.enable = false;
-
-      # Fine-grained power management. Turns off GPU when not in use.
-      powerManagement.finegrained = false;
-
-      # Use the NVidia open source kernel module (not to be confused with the
-      # independent third-party "nouveau" open source driver).
-      # Support is limited to the Turing and later architectures. Full list of 
-      # supported GPUs is at: 
-      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-      # Only available from driver 515.43.04+
-      # Currently alpha-quality/buggy, so false is currently the recommended setting.
-      open = true;
-
-# TODO : correct that part with cfg.
-#      prime = {
-#        intelBusId = lib.optionalAttrs (cfg.intelBusId != null) cfg.intelBusId;
-#        nvidiaBusId = lib.optionalAttrs (cfg.nvidiaBusId != null) cfg.nvidiaBusId;
-#        amdgpuBusId = lib.optionalAttrs (cfg.amdgpuBusId != null) cfg.amdgpuBusId;
-#      };
-
-    };
 
   };  
 }
