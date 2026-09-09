1ae2e2d795c13561a5e817f30e78928649a62304
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-09 16:37:37 +0200

    Update bluetooth.nix

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -25,19 +25,7 @@
      hardware.bluetooth = {
         enable = true;
         powerOnBoot = true;
-        package = pkgs.bluez5-experimental;
-        settings.Policy.AutoEnable = "true";
-        settings.General.Enable = "Source,Sink,Media,Socket";
      };
-
-     services.blueman.enable = true;
-
-     environment.systemPackages = with pkgs; [
-          bluez5-experimental 
-          bluez-tools
-          bluez-alsa
-          bluetuith # can transfer files via OBEX
-        ];
     
   };  
 }
