eec15f6c7f09e642c603fb295f7ca3ea89f0e477
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-12 23:57:27 +0200

    Update system-gocryptfs.nix

--- modules/services/system-gocryptfs.nix
+++ modules/services/system-gocryptfs.nix
@@ -61,10 +61,6 @@
         Type = "forking";
         RemainAfterExit = true;
         EnvironmentFile = [ "/home/${user}/.mounts/%i.env" ];
-        Environment = [
-          "WAYLAND_DISPLAY=wayland-0"
-          "XDG_RUNTIME_DIR=/run/user/${uid}"
-        ];
         ExecStartPre = ''
           ${pkgs.bash}/bin/bash -c \
             'test -d "$CIPHERDIR" || exit 0 \
