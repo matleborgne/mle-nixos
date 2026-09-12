271cbe13731fe09d556ae3596d6559a200fc2ce4
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-12 15:48:44 +0200

    Update firefox-arch.nix

--- modules/containers/podman/firefox-arch.nix
+++ modules/containers/podman/firefox-arch.nix
@@ -29,7 +29,7 @@
       allUsers = builtins.attrNames config.users.users;
       normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
       user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
-      uid = config.users.users.${user}.uid;
+      uid = toString config.users.users.${user}.uid;
 
     in {
 
@@ -53,7 +53,8 @@
           User = user;     
         };
         script = ''
-          podman build -t localhost/${cname} -f ${containerfile}
+          podman build -t localhost/${cname} -f ${containerfile} \
+            --build-arg UID=${uid} --build-arg GID=${uid} --build-arg UNAME=${user}
         '';
         wantedBy = [ "multi-user.target" ];
       };
@@ -68,22 +69,22 @@
         containers.${cname} = {
           image = "localhost/${cname}";
           autoStart = false;
-          user = "1000:1000";
+          user = "${uid}:${uid}";
 
           environment = {
-            PULSE_SERVER       = "unix:/run/user/1000/pulse/native";
+            PULSE_SERVER       = "unix:/run/user/${uid}/pulse/native";
             XDG_DATA_DIRS      = "/usr/share";
             MOZ_ENABLE_WAYLAND = "1";
             WAYLAND_DISPLAY    = "wayland-0";
-            XDG_RUNTIME_DIR    = "/run/user/1000";
+            XDG_RUNTIME_DIR    = "/run/user/${uid}";
             HOME               = "/home/${user}";
           };
 
           volumes = [
-            "/run/user/1000/wayland-0:/run/user/1000/wayland-0:U"
-            "/run/user/1000/pulse:/run/user/1000/pulse:U"
-            "/run/user/1000/pipewire-0:/run/user/1000/pipewire-0:U"
-            "/run/user/1000/dconf:/run/user/1000/dconf:U"
+            "/run/user/${uid}/wayland-0:/run/user/${uid}/wayland-0:U"
+            "/run/user/${uid}/pulse:/run/user/${uid}/pulse:U"
+            "/run/user/${uid}/pipewire-0:/run/user/${uid}/pipewire-0:U"
+            "/run/user/${uid}/dconf:/run/user/${uid}/dconf:U"
           ];
 
           extraOptions = [
