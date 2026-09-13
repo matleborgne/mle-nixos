16e2bba8cd81154827cb7c64061b0322a7142269
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-13 15:29:05 +0200

    Update firefox-arch.nix

--- modules/containers/podman/firefox-arch.nix
+++ modules/containers/podman/firefox-arch.nix
@@ -19,16 +19,40 @@
     let
       cname = "firefox-arch";
 
+      allUsers = builtins.attrNames config.users.users;
+      normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
+      user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
+      uid = toString config.users.users.${user}.uid;
+
       containerfile = pkgs.writeText "Containerfile" ''
         FROM archlinux
         RUN pacman -Syu --noconfirm firefox xkeyboard-config libpulse
         RUN pacman -Scc --noconfirm && rm -rf /var/cache/pacman/pkg/*
       '';
 
-      allUsers = builtins.attrNames config.users.users;
-      normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) allUsers;
-      user = (if builtins.length normalUsers > 0 then builtins.elemAt normalUsers 0 else "root");
-      uid = toString config.users.users.${user}.uid;
+      quadletfile = pkgs.writeText "Quadletfile" ''
+        [Container]
+        ContainerName=firefox-arch
+        Image=firefox-arch
+        User=${uid}
+        Group=${uid}
+
+        PodmanArgs=--interactive --tty --userns=keep-id --read-only
+        DropCapability=CAP_AUDIT_WRITE CAP_MKNOD CAP_NET_RAW
+        NoNewPrivileges=true
+        Network=pasta
+
+        Environment=WAYLAND_DISPLAY XDG_RUNTIME_DIR HOME
+        Environment=PULSE_SERVER=unix:/run/user/${uid}/pulse/native
+        Environment=XDG_DATA_DIRS=/usr/share
+        Environment=MOZ_ENABLE_WAYLAND=1
+
+        Volume=/run/user/${uid}/wayland-0:/run/user/${uid}/wayland-0:U
+        Volume=/run/user/${uid}/pulse:/run/user/${uid}/pulse:U
+        Volume=/run/user/${uid}/dconf:/run/user/${uid}/dconf:U
+
+        Exec=/usr/bin/firefox --name firefox-arch
+      '';
 
     in {
 
