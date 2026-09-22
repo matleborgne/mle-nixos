935132a09479e89290735c16c01f05b1beae4cbd
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2026-09-22 10:35:52 +0200

    Update codium.nix

--- modules/apps/codium.nix
+++ modules/apps/codium.nix
@@ -93,7 +93,8 @@
       install -o "${user}" -g users -m 0644 ${settingsJson} "$dir/settings.json"
       install -o "${user}" -g users -m 0644 ${localeJson} "$dir/locale.json"
 
-      ${pkgs.util-linux}/bin/runuser -u "${user}" ${pythonEnv}/bin/python -m ipykernel install \
+      ${pkgs.util-linux}/bin/runuser -u "${user}" -- \
+        ${pythonEnv}/bin/python -m ipykernel install \
         --user --name=nixos-pandas --display-name "NixOS (pandas)" --force \
         >> /home/${user}/.cache/ipykernel-install.log 2>&1 || true
     '';
